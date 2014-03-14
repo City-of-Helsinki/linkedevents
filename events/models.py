0df4bcfb1dae33925520303d9ec7ba6a6fcf139f
Timo Hanhirova <timo.hanhirova@arcusys.fi>
2014-03-14 15:42:38 +0200

    Add contained_in relation to Place

--- events/models.py
+++ events/models.py
@@ -130,7 +130,7 @@ class PostalAddress(BaseModel):
     available_language = models.ForeignKey(Language, db_index=True)
 
 
-class Place(BaseModel, SchemalessFieldMixin):
+class Place(MPTTModel, BaseModel, SchemalessFieldMixin):
     same_as = models.CharField(max_length=255, null=True, blank=True)
     description = models.TextField(null=True)
     address = models.ForeignKey(PostalAddress, null=True, blank=True)
@@ -139,15 +139,15 @@ class Place(BaseModel, SchemalessFieldMixin):
     point = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     logo = models.CharField(max_length=255, null=True, blank=True)
     map = models.CharField(max_length=255, null=True, blank=True)
-    #contained_in = TreeForeignKey('self', null=True, blank=True, related_name='children')
+    contained_in = TreeForeignKey('self', null=True, blank=True, related_name='children')
     creator = models.ForeignKey(Person, null=True, blank=True, related_name='place_creators')  # TODO: Person or Organization
     editor = models.ForeignKey(Person, null=True, blank=True, related_name='place_editors')  # TODO: Person or Organization
 
     class Meta:
         verbose_name = _('Place')
 
-    # class MPTTMeta:
-    #     parent_attr = 'contained_in'
+    class MPTTMeta:
+        parent_attr = 'contained_in'
 
 reversion.register(Place)
 
