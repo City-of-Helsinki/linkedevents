32f772e62c85cf06570ec3470464ceffdfb3a15b
Timo Tuominen <tituomin@gmail.com>
2014-04-04 21:14:08 +0000

    UTF fix.

--- events/importer/ysa.py
+++ events/importer/ysa.py
@@ -9,8 +9,8 @@ from rdflib.namespace import FOAF, SKOS
 
 from events.models import *
 
-from util import active_language
-
+from sys import stdout
+from .util import active_language
 from .sync import ModelSyncher
 from .base import Importer, register_importer
 
@@ -30,17 +30,21 @@ class YsaImporter(Importer):
         requests_cache.install_cache('ysa')
 
     def import_categories(self):
-        print("Importing YSA and Allärs categories")
-        for lang, url in URLS.iteritems():
+        stdout.write("Importing YSA and Allärs categories")
+        for lang, url in URLS.items():
             resp = requests.get(url)
             assert resp.status_code == 200
+            resp.encoding = 'UTF-8'
             graph = rdflib.Graph()
             graph.parse(data=resp.text, format='turtle')
             self.save_categories(lang, graph)
 
     def save_categories(self, lang, graph):
         with active_language(lang):
+#            i = 0
             for subject in graph.subjects(RDF.type, SKOS.Concept):
+#                i += 1
+#                if i == 1000: break
                 self.save_category(graph, subject)
 
     def save_category(self, graph, subject):
@@ -52,21 +56,22 @@ class YsaImporter(Importer):
             parent_category = None
         else:
             parent_category = self.save_category(graph, parent_category_subject)
+        translation_objects = graph.objects(
+            subject=subject,
+            predicate=SKOS.exactMatch
+        )
+        ocount = len(list(translation_objects))
+        if ocount > 1:
+            print('exceptional translations: %d %s' % (ocount, str(subject)))
         translation_subject = graph.value(
             subject=subject,
             predicate=SKOS.exactMatch
         )
-        try:
-            translation = Category.objects.get(url=translation_subject)
-        except Category.DoesNotExist:
-            translation = None
-        if not Category.objects.filter(url=unicode(subject)).exists():
-            category=Category(
-                url=unicode(subject),
-                label=graph.preferredLabel(subject)[0][1],
-                parent_category=parent_category,
-                same_as=translation
-            )
-            category.save()
-            return category
-        return None
+        category = Category.objects.get_or_create(
+            url=str(subject),
+            defaults={
+                'label': graph.preferredLabel(subject)[0][1],
+                'parent_category': parent_category,
+                'same_as': str(translation_subject)
+            }
+        )
