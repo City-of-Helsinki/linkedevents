e8fd7bc13edc72c9e0080bb25482edd1c0608be0
Juha Yrjölä <juha.yrjola@iki.fi>
2014-04-07 15:02:41 +0300

    API lives now at /v0.1

--- linkedevents/urls.py
+++ linkedevents/urls.py
@@ -5,8 +5,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^v1/', include('events.urls')),
+    url(r'^v0.1/', include('events.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^docs/', include('rest_framework_swagger.urls')),
-    url(r'^$', RedirectView.as_view(url='v1/', permanent=False)),
+    url(r'^$', RedirectView.as_view(url='/v0.1/', permanent=False)),
+    url(r'^v1', RedirectView.as_view(url='/v0.1/', permanent=False)),
 )
