d7ba5a6d685c2022f4aca6290bdbebc90d6ec9cf
Juha Yrjölä <juha.yrjola@iki.fi>
2014-04-10 15:25:19 +0300

    Fix settings.BASE_DIR

--- linkedevents/settings/base.py
+++ linkedevents/settings/base.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
