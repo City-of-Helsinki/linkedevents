00d6671e7eebc6de5c77789100e89d0beb54b10a
Timo Tuominen <tituomin@gmail.com>
2014-06-30 11:23:15 +0000

    Add new SearchQuerySet with decay functions.
    Enables e.g. decaying the search results by a date field.

--- events/custom_elasticsearch_search_backend.py
+++ events/custom_elasticsearch_search_backend.py
@@ -1,5 +1,6 @@
 import collections
 from haystack.backends import elasticsearch_backend as es_backend
+from haystack.query import SearchQuerySet
 from .utils import update
 
 class CustomEsSearchBackend(es_backend.ElasticsearchSearchBackend):
@@ -37,5 +38,50 @@ class CustomEsSearchBackend(es_backend.ElasticsearchSearchBackend):
                     target[key] = value
         return (content_field_name, mappings)
 
+    def build_search_kwargs(self, query_string, decay_functions=None, **kwargs):
+        kwargs = super(CustomEsSearchBackend, self).build_search_kwargs(query_string, **kwargs)
+        if not decay_functions:
+            return kwargs
+
+        original_query = kwargs['query']
+        function_score_query = {
+            'function_score': {
+                'functions': decay_functions,
+                'query': original_query,
+                'score_mode': 'multiply'
+            }
+        }
+        kwargs['query'] = function_score_query
+        return kwargs
+
+class CustomEsSearchQuery(es_backend.ElasticsearchSearchQuery):
+    def __init__(self, **kwargs):
+        super(CustomEsSearchQuery, self).__init__(**kwargs)
+        self.decay_functions = []
+
+    def build_params(self, *args, **kwargs):
+        search_kwargs = super(CustomEsSearchQuery, self).build_params(*args, **kwargs)
+        if self.decay_functions:
+            search_kwargs['decay_functions'] = self.decay_functions
+        return search_kwargs
+
+    def add_decay_function(self, function_dict):
+        self.decay_functions.append(function_dict)
+
+
+class CustomEsSearchQuerySet(SearchQuerySet):
+    """
+    usage example:
+    SearchQuerySet().filter(text='konsertti').decay({'gauss': {'end_time' : {'origin': '2014-05-07', 'scale' : '10d' }}}
+    """
+
+    def decay(self, function_dict):
+        clone = self._clone()
+        clone.query.add_decay_function(function_dict)
+        return clone
+
+
 class CustomEsSearchEngine(es_backend.ElasticsearchSearchEngine):
     backend = CustomEsSearchBackend
+    query = CustomEsSearchQuery
+
