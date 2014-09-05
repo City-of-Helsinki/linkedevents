bd33a535d68841873df4635aa7441b3c7b04c5f6
Timo Tuominen <tituomin@gmail.com>
2014-09-05 18:51:36 +0300

    Make translation utils support untranslated models.

--- events/translation_utils.py
+++ events/translation_utils.py
@@ -1,16 +1,20 @@
+import modeltranslation
 from modeltranslation.translator import translator
 
 def expand_model_fields(model, field_names):
     model_class = type(model)
-    trans_field_mapping = translator.get_options_for_model(model_class).fields
+    try:
+        trans_field_mapping = translator.get_options_for_model(model_class).fields
+    except modeltranslation.translator.NotRegistered:
+        return field_names
 
     def expand_field(field_name):
-        translated_versions = trans_field_mapping.get(
-            field_name,
-            [model._meta.get_field(field_name)]
-        )
-        return (f for f in translated_versions)
+        translated_versions = trans_field_mapping.get(field_name)
+        if translated_versions is not None:
+            return (f.name for f in translated_versions)
+        else:
+            return [field_name]
 
-    return [expanded.name
+    return [expanded
             for unexpanded in field_names
             for expanded in expand_field(unexpanded)]
