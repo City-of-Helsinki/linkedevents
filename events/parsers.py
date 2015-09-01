5d8b48f7f5f9c9c779df9371c4049ef8cc9dec62
Aapo Rista <aapo.gitpub@rista.net>
2015-09-01 10:08:57 +0300

    Change QUERY_PARAMS -> query_params

--- events/parsers.py
+++ events/parsers.py
@@ -28,7 +28,7 @@ def rename_fields(dataz):
 class CamelCaseJSONParser(JSONParser):
     def parse(self, stream, media_type=None, parser_context=None):
         parser_context = parser_context or {}
-        if 'disable_camelcase' in parser_context['request'].QUERY_PARAMS:
+        if 'disable_camelcase' in parser_context['request'].query_params:
             return super(CamelCaseJSONParser, self).parse(media_type,
                                                           parser_context)
         else:
