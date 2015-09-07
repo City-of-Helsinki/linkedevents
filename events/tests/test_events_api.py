671d97fe4851f20bbe1178bd0c3f6df2d94da76e
tommi <zenbum@users.noreply.github.com>
2015-09-07 11:22:56 +0300

    Contributes to #1.

---
+++ events/tests/test_events_api.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# django
+from django.test import TestCase
+
+
+class EventAPITests(TestCase):
+
+    def test__create_event_with_put(self):
+        self.assertEqual(True, False)
