662b250b1c1b0162805649378d971ef64f06ce4e
Rikuoja <riku.oja@kapsi.fi>
2016-02-08 17:31:46 +0200

    Fix the two image url migrations

--- events/models.py
+++ events/models.py
@@ -332,7 +332,7 @@ class Event(MPTTModel, BaseModel, SchemalessFieldMixin):
     keywords = models.ManyToManyField(Keyword)
     audience = models.ManyToManyField(Keyword, related_name='audiences', blank=True)
 
-    external_image_url = models.URLField(max_length=400)
+    external_image_url = models.URLField(max_length=400, null=True, blank=True)
 
     class Meta:
         verbose_name = _('event')
