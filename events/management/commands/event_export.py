56aaee328a2a5b44dc46c23ff3043ef9ff293b45
Tuomas Haapala <tuomas.haapala@anders.fi>
2016-08-05 14:28:06 +0300

    Replace deprecated optparse with argparse in management commands
    Closes #75

--- events/management/commands/event_export.py
+++ events/management/commands/event_export.py
@@ -8,55 +8,50 @@ from events.exporter.base import get_exporters
 
 
 class Command(BaseCommand):
-    args = '<module>'
     help = "Export event data"
-    option_list = list(BaseCommand.option_list + (
-        make_option('--new', action='store_true', dest='new',
-                    help='Export entities added after last export date'),
-        make_option('--delete', action='store_true', dest='delete',
-                    help='Delete exported items from target system'),
-    ))
 
     exporter_types = ['events']
 
     def __init__(self):
-        super(Command, self).__init__()
-        for imp in self.exporter_types:
-            opt = make_option('--%s' % imp, dest=imp, action='store_true',
-                              help='export %s' % imp)
-            self.option_list.append(opt)
+        super().__init__()
+        self.exporters = get_exporters()
+        self.exp_list = ', '.join(sorted(self.exporters.keys()))
+        self.missing_args_message = "Enter the name of the event exporter module. Valid exporters: %s" % self.exp_list
+
+    def add_arguments(self, parser):
+        parser.add_argument('module')
+        parser.add_argument('--new', action='store_true', dest='new',
+                            help='Export entities added after last export date')
+        parser.add_argument('--delete', action='store_true', dest='delete',
+                            help='Delete exported items from target system')
+        for exp in self.exporter_types:
+            parser.add_argument('--%s' % exp, dest=exp, action='store_true', help='export %s' % exp)
 
     def handle(self, *args, **options):
-        importers = get_exporters()
-        imp_list = ', '.join(sorted(importers.keys()))
-        if len(args) != 1:
-            raise CommandError(
-                "Enter the name of the event exporter module. "
-                "Valid exporters: %s" % imp_list)
-        if not args[0] in importers:
-            raise CommandError("Exporter %s not found. Valid exporters: %s"
-                               % (args[0], imp_list))
-        imp_class = importers[args[0]]
+        module = options['module']
+        if not module in self.exporters:
+            raise CommandError("Exporter %s not found. Valid exporters: %s" % (module, self.exp_list))
+        exp_class = self.exporters[module]
 
         if hasattr(settings, 'PROJECT_ROOT'):
             root_dir = settings.PROJECT_ROOT
         else:
             root_dir = settings.BASE_DIR
-        exporter = imp_class()
+        exporter = exp_class()
 
-        # Activate the default language for the duration of the import
+        # Activate the default language for the duration of the export
         # to make sure translated fields are populated correctly.
         old_lang = get_language()
         activate(settings.LANGUAGES[0][0])
 
-        for imp_type in self.exporter_types:
-            name = "export_%s" % imp_type
+        for exp_type in self.exporter_types:
+            name = "export_%s" % exp_type
             method = getattr(exporter, name, None)
-            if options[imp_type]:
+            if options[exp_type]:
                 if not method:
                     raise CommandError(
                         "Exporter %s does not support exporter %s"
-                        % (name, imp_type))
+                        % (name, exp_type))
             else:
                 if not options['new'] and not options['delete']:
                     continue
