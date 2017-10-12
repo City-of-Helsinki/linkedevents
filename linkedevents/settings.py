fbb7d7a20c7d2a7ee042faaf4743a3c2f878d236
Tomi Hukkalainen <tomi.hukkalainen@iki.fi>
2017-10-12 10:42:46 +0300

    Ignore imports further down the files

--- linkedevents/settings.py
+++ linkedevents/settings.py
@@ -232,10 +232,10 @@ HAYSTACK_CONNECTIONS = {
     }
 }
 
-import bleach
+import bleach  # noqa
 BLEACH_ALLOWED_TAGS = bleach.ALLOWED_TAGS + ["p", "div"]
 
-from easy_thumbnails.conf import Settings as thumbnail_settings
+from easy_thumbnails.conf import Settings as thumbnail_settings  # noqa
 THUMBNAIL_PROCESSORS = (
     'image_cropping.thumbnail_processors.crop_corners',
 ) + thumbnail_settings.THUMBNAIL_PROCESSORS
