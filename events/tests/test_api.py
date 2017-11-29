60be9c169d7ea23153464065ff43c93dc026a489
Mingfeng Yu <mingfeng.yu@anders.fi>
2017-11-29 11:09:00 +0200

    Allow fetching objects published by replaced/replacing organization
    When filtering the objects with publishers, the REST API will also return objects that are published by a replaced organization when a replacing organizaion is provided. Also it will return objects for replacing organization if a replaced organization is provided.

--- events/tests/test_api.py
+++ events/tests/test_api.py
@@ -1,5 +1,12 @@
+from unittest.mock import MagicMock, patch
+
 import pytest
+from django_orghierarchy.models import Organization
+from rest_framework import status
+from rest_framework.test import APITestCase
+
 from .utils import versioned_reverse as reverse
+from ..models import DataSource, Event, Image, PublicationStatus
 
 
 @pytest.mark.django_db
@@ -19,3 +26,153 @@ def test_api_page_size(api_client, event):
     assert resp.status_code == 200
     meta = resp.data['meta']
     assert len(resp.data['data']) <= 100
+
+
+class TestImageAPI(APITestCase):
+
+    def setUp(self):
+        data_source = DataSource.objects.create(
+            id='ds',
+            name='data-source',
+        )
+        org_1 = Organization.objects.create(
+            name='org-1',
+            origin_id='org-1',
+            data_source=data_source,
+        )
+        org_2 = Organization.objects.create(
+            name='org-2',
+            origin_id='org-2',
+            data_source=data_source,
+            replaced_by=org_1,
+        )
+        org_3 = Organization.objects.create(
+            name='org-3',
+            origin_id='org-3',
+            data_source=data_source,
+        )
+        self.image_1 = Image.objects.create(
+            name='image-1',
+            data_source=data_source,
+            publisher=org_1,
+            url='http://fake.url/image-1/',
+        )
+        self.image_2 = Image.objects.create(
+            name='image-2',
+            data_source=data_source,
+            publisher=org_2,
+            url='http://fake.url/image-2/',
+        )
+        self.image_3 = Image.objects.create(
+            name='image-2',
+            data_source=data_source,
+            publisher=org_3,
+            url='http://fake.url/image-2/',
+        )
+
+    def test_get_image_list_with_publisher(self):
+        # test filtering with replaced organization
+        url = '{0}?publisher=ds:org-1'.format(reverse('image-list'))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['data']), 2)
+
+        # test filtering with organization that replaces organization
+        url = '{0}?publisher=ds:org-2'.format(reverse('image-list'))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['data']), 2)
+
+        # test filtering with normal organization
+        url = '{0}?publisher=ds:org-3'.format(reverse('image-list'))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['data']), 1)
+
+
+class TestEventAPI(APITestCase):
+
+    def setUp(self):
+        self.data_source = DataSource.objects.create(
+            id='ds',
+            name='data-source',
+        )
+        self.org_1 = Organization.objects.create(
+            name='org-1',
+            origin_id='org-1',
+            data_source=self.data_source,
+        )
+        self.org_2 = Organization.objects.create(
+            name='org-2',
+            origin_id='org-2',
+            data_source=self.data_source,
+            replaced_by=self.org_1,
+        )
+        self.org_3 = Organization.objects.create(
+            name='org-3',
+            origin_id='org-3',
+            data_source=self.data_source,
+        )
+        self.event_1 = Event.objects.create(
+            id='event-1',
+            name='event-1',
+            data_source=self.data_source,
+            publisher=self.org_1,
+            publication_status=PublicationStatus.DRAFT,
+        )
+        self.event_2 = Event.objects.create(
+            id='event-2',
+            name='event-2',
+            data_source=self.data_source,
+            publisher=self.org_2,
+            publication_status=PublicationStatus.DRAFT,
+        )
+        self.event_3 = Event.objects.create(
+            id='event-3',
+            name='event-3',
+            data_source=self.data_source,
+            publisher=self.org_3,
+            publication_status=PublicationStatus.DRAFT,
+        )
+        self.event_4 = Event.objects.create(
+            id='event-4',
+            name='event-4',
+            data_source=self.data_source,
+            publisher=self.org_3,
+            publication_status=PublicationStatus.PUBLIC,
+        )
+
+    def test_event_list_with_auth_filters(self):
+        # test with public request
+        url = '{0}?show_all=1'.format(reverse('event-list'))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['data']), 1)  # event-4
+
+        # test with authenticated data source and publisher
+        with patch(
+            'events.api.get_authenticated_data_source_and_publisher',
+            MagicMock(return_value=(self.data_source, self.org_2))
+        ):
+            url = '{0}?show_all=1'.format(reverse('event-list'))
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(len(response.data['data']), 3)  # event-1, event-2 and event-4
+
+    def test_event_list_with_publisher_filters(self):
+        # test with public request
+        url = '{0}?show_all=1&publisher=ds:org-3'.format(reverse('event-list'))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['data']), 1)  # event-4
+
+        # test with authenticated data source and publisher
+        with patch(
+            'events.api.get_authenticated_data_source_and_publisher',
+            MagicMock(return_value=(self.data_source, self.org_2))
+        ):
+            url = '{0}?show_all=1&publisher=ds:org-2'.format(reverse('event-list'))
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            # note that org-2 is replaced by org-1
+            self.assertEqual(len(response.data['data']), 2)  # event-1 and event-2
