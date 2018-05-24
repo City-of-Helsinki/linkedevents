b55c5de7c8293458828db9cc525b7f5b0faf7064
Ville Koivunen <ville.koivunen@keskiespoo.net>
2018-05-24 22:52:15 +0300

    Flake8 is a pretty good thing actually

--- events/tests/conftest.py
+++ events/tests/conftest.py
@@ -15,7 +15,6 @@ from munigeo.models import (AdministrativeDivision, AdministrativeDivisionType, 
 import pytest
 from rest_framework.test import APIClient
 from django_orghierarchy.models import Organization
-from django.core.management import call_command
 
 # events
 from events.models import (
@@ -34,19 +33,16 @@ DATETIME = (timezone.now() + timedelta(days=1)).isoformat().replace('+00:00', 'Z
 
 OTHER_DATA_SOURCE_ID = "testotherdatasourceid"
 
+
 # Django test harness tries to serialize DB in order to support transactions
 # within tests. (It restores the snapshot after such tests).
 # This fails with modeltranslate, as the serialization is done before
-# sync_translation_fields has a change to run. Thus the fields are missing
+# sync_translation_fields has a chance to run. Thus the fields are missing
 # and serialization fails horribly.
 @pytest.fixture(scope='session')
 def django_db_modify_db_settings(django_db_modify_db_settings_xdist_suffix):
     settings.DATABASES['default']['TEST']['SERIALIZE'] = False
 
-@pytest.fixture(scope='session')
-def django_db_setup(django_db_setup, django_db_blocker):
-    with django_db_blocker.unblock():
-        call_command('sync_translation_fields','--noinput')
 
 @pytest.fixture(scope='session')
 def django_db_setup(django_db_setup, django_db_blocker):
