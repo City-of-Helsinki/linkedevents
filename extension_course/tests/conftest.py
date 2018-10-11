96b20c8a670fc99fdeace5cd0bbff8f9a6d1bd90
Riku Oja <riku.oja@kapsi.fi>
2018-10-11 13:45:07 +0300

    Add extension_course tests to travis

--- extension_course/tests/conftest.py
+++ extension_course/tests/conftest.py
@@ -1,9 +1,3 @@
-import pytest
 from events.tests.conftest import (administrative_division, administrative_division_type, data_source, event,  # noqa
                                    location_id, minimal_event_dict, municipality, organization, place, user,
                                    user_api_client, django_db_modify_db_settings, django_db_setup)
-
-
-@pytest.fixture(autouse=True)
-def auto_enable(settings):
-    settings.AUTO_ENABLED_EXTENSIONS = ['course']
