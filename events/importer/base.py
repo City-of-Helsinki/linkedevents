2b4f3a30ef33cadf4f5db7874b2177c84cec6924
Riku Oja <riku.oja@kapsi.fi>
2018-11-12 17:15:39 +0200

    Cleaner image delete function in base importer

--- events/importer/base.py
+++ events/importer/base.py
@@ -3,6 +3,7 @@ import logging
 import itertools
 import datetime
 from collections import defaultdict
+from functools import partial
 import operator
 
 from django.conf import settings
@@ -102,7 +103,7 @@ class Importer(object):
     def set_images(self, obj, images_data):
         image_syncher = ModelSyncher(obj.images.all(),
                                      lambda image: image.url,
-                                     delete_func=self._image_remove_func(obj))
+                                     delete_func=partial(self._remove_image, obj))
 
         for image_data in images_data:
             image_url = image_data.get('url', '').strip()
@@ -132,10 +133,8 @@ class Importer(object):
     def _remove_image(self, obj, image):
         # we need this to mark the object changed if an image is removed
         obj._changed = True
-        return obj.images.remove(image)
-
-    def _image_remove_func(self, obj):
-        return lambda image: self._remove_image(obj, image)
+        obj.images.remove(image)
+        return True
 
     def _get_image(self, image_url):
         if not image_url:
