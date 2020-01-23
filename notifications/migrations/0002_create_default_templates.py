88e67b67b5a5ec68e10d7125766a13a5a3dae5b8
Tatu Rajaniemi <tatu.rajaniemi@haltu.fi>
2020-01-23 16:46:25 +0200

    Add Swedish and English default templates

--- notifications/migrations/0002_create_default_templates.py
+++ notifications/migrations/0002_create_default_templates.py
@@ -7,10 +7,12 @@ NOTIFICATION_TYPES = (
     'event_published',
     'draft_posted'
 )
-LANGUAGES = ['fi']
+LANGUAGES = ['fi', 'sv', 'en']
 DEFAULT_LANGUAGE = 'fi'
 
 FOOTER_FI = 'Tämä on automaattinen viesti Helsingin kaupungin tapahtumarajapinnasta. Viestiin ei voi vastata.\n'
+FOOTER_SV = 'Detta är ett automatiskt meddelande från Helsingfors stads evenemangsgränssnitt. Det är inte möjligt att svara på det här meddelandet.\n'
+FOOTER_EN = 'This is an automatic message from the City of Helsinki’s Event API. This is a no-reply message.\n'
 
 HTML_SEPARATOR = '\n<br/><br/>\n'
 
@@ -22,6 +24,26 @@ Ilmoituksesi poistettiin, joko 1) koska se ei noudattanut Helsingin kaupungin <a
 <br/><br/>
 Jos haluat lisätietoja, voit jättää asiasta kysymyksen osoitteessa <a href="https://hel.fi/palaute">hel.fi/palaute.</a> Mainitse palautteessasi tapahtuman nimi ja julkaisuaika: "{{ event.name }}", {{ event.created_time|format_datetime('fi') }}"""
 
+UNPUBLISHED_EVENT_DELETED_SUBJECT_SV = 'Din evenemangsanmälan "{{ event.name }}" har strukits – Helsingfors stad'
+UNPUBLISHED_EVENT_DELETED_HTML_BODY_SV = \
+"""Evenemanget "{{ event.name }}" som du har anmält till Helsingfors stad {{ event.created_time|format_datetime('sv') }} har strukits.
+<br/><br/>
+Din anmälan ströks antingen 1) eftersom det inte <a href="https://linkedevents.hel.fi/terms">uppfyllde användarvillkoren för Helsingfors stads evenemangsgränssnitt</a> eller 2) eftersom det av annan orsak inte ansågs lämpa sig för stadens evenemangsgränssnitt.
+<br/><br/>
+Om du vill ha ytterligare upplysningar, kan du lämna en fråga om saken på <a href="https://hel.fi/respons">hel.fi/respons</a>. I din respons, nämn evenemangets namn och publiceringstid:
+<br/><br/>
+"{{ event.name }}", {{ event.created_time|format_datetime('sv') }}"""
+
+UNPUBLISHED_EVENT_DELETED_SUBJECT_EN = 'Your event registration "{{ event.name }}" has been removed – City of Helsinki'
+UNPUBLISHED_EVENT_DELETED_HTML_BODY_EN = \
+"""The "{{ event.name }}" event that you registered to the City of Helsinki on {{ event.created_time|format_datetime('en') }} has been removed.
+<br/><br/>
+You registration was removed either 1) because it did not adhere to <a href="https://linkedevents.hel.fi/terms">the terms and conditions of the City of Helsinki’s Event API</a> or 2) because the event was not deemed suitable for the City's Event API for another reason.
+<br/><br/>
+If you want further information, you can submit a question about it on <a href="https://hel.fi/feedback">hel.fi/feedback</a>. In your feedback, please mention the event’s name and time of publication:
+<br/><br/>
+"{{ event.name }}", {{ event.created_time|format_datetime('en') }}"""
+
 EVENT_PUBLISHED_SUBJECT_FI = 'Tapahtumailmoituksesi "{{ event.name }}" on julkaistu – Helsingin kaupunki'
 EVENT_PUBLISHED_HTML_BODY_FI = \
 """Helsingin kaupungille {{ event.created_time|format_datetime('fi') }} ilmoittamasi tapahtuma "{{ event.name }}" on julkaistu.
@@ -29,6 +51,26 @@ EVENT_PUBLISHED_HTML_BODY_FI = \
 Tapahtuma tulee näkyville <a href="https://hel.fi/tapahtumat">Helsingin tapahtumakalenteriin</a> enintään tunnin viiveellä. Tapahtuma voidaan näyttää myös muissa kalentereissa, jotka hakevat tietoja Helsingin kaupungin tapahtumarajapinnasta.
 <br/><br/>
 Et voi enää muokata tapahtumaa julkaisun jälkeen. Jos haluat muuttaa tapahtuman tietoja, jätä muutospyyntö osoitteessa <a href="https://hel.fi/palaute">hel.fi/palaute</a>. Mainitse palautteessasi tapahtuman nimi ja julkaisuaika:"{{ event.name }}", {{ event.created_time|format_datetime('fi') }}"""
+
+EVENT_PUBLISHED_SUBJECT_SV = 'Din evenemangsanmälan "{{ event.name }}" har publicerats – Helsingfors stad'
+EVENT_PUBLISHED_HTML_BODY_SV = \
+"""Evenemanget ”{{ event.name }}” som du har anmält till Helsingfors stad {{ event.created_time|format_datetime('sv') }} har publicerats.
+<br/><br/>
+Evenemanget publiceras i <a href="https://hel.fi/tapahtumat">Helsingfors evenemangskalender</a> med högst en timmes fördröjning. Evenemanget kan visas också i andra kalendrar som söker data ur Helsingfors stads evenemangsgränssnitt.
+<br/><br/>
+Du kan inte längre redigera evenemanget efter att det har publicerats. Om du vill ändra på uppgifterna för evenemanget, lämna en begäran om ändring på adressen hel.fi/respons. I din respons, nämn evenemangets namn och publiceringstid:
+<br/><br/>
+"{{ event.name }}", {{ event.created_time|format_datetime('sv') }}"""
+
+EVENT_PUBLISHED_SUBJECT_EN = 'Your event registration "{{ event.name }}" has been published – City of Helsinki'
+EVENT_PUBLISHED_HTML_BODY_EN = \
+"""The "{{ event.name }}" event that you registered to the City of Helsinki on {{ event.created_time|format_datetime('en') }} has been published.
+<br/><br/>
+The event will become visible <a href="https://hel.fi/tapahtumat">in Helsinki’s event calendar</a> within an hour at most. The event can also be shown in other calendars, which look up data from the City of Helsinki’s Event API.
+<br/><br/>
+You can no longer edit the event when it has been published. If you want to change the event data, submit a change request on hel.fi/feedback. In your feedback, please mention the event’s name and time of publication:
+<br/><br/>
+"{{ event.name }}", {{ event.created_time|format_datetime('en') }}"""
 
 DRAFT_POSTED_SUBJECT_FI = 'Uusi tapahtumaluonnos "{{ event.name }}", {{ event.created_time|format_datetime(\'fi\') }} – Helsingin kaupungin tapahtumarajapinta'
 DRAFT_POSTED_HTML_BODY_FI = \
@@ -39,6 +81,26 @@ DRAFT_POSTED_HTML_BODY_FI = \
 <a href="https://linkedevents.hel.fi/event/{{ event.id }}">Siirry moderoimaan tapahtumia »</a>
 <br/><br/>
 Sait tämän viestin, koska olet moderaattori organisaatiossa {{ event.publisher.name }}."""
+
+DRAFT_POSTED_SUBJECT_SV = 'Nytt evenemangsutkast "{{ event.name }}", {{ event.created_time|format_datetime(\'sv\') }} – Helsingfors stads evenemangsgränssnitt'
+DRAFT_POSTED_HTML_BODY_SV = \
+"""Ett nytt evenemangsutkast har skapats i Helsingfors stads evenemangsgränssnitt.
+<br/><br/>
+"{{ event.name }}", {{ event.created_time|format_datetime('sv') }}
+<br/><br/>
+<a href="https://linkedevents.hel.fi/event/{{ event.id }}">Siirry moderoimaan tapahtumia »</a>
+<br/><br/>
+Du fick det här meddelandet eftersom du är moderator i organisationen {{ event.publisher.name }}."""
+
+DRAFT_POSTED_SUBJECT_EN = 'New event draft "{{ event.name }}", {{ event.created_time|format_datetime(\'en\') }} – City of Helsinki Event API'
+DRAFT_POSTED_HTML_BODY_EN = \
+"""A new event draft has been created in the City of Helsinki’s Event API:
+<br/><br/>
+"{{ event.name }}", {{ event.created_time|format_datetime('en') }}
+<br/><br/>
+<a href="https://linkedevents.hel.fi/event/{{ event.id }}">Go to moderate events »</a>
+<br/><br/>
+You received this message because you are a moderator in the {{ event.publisher.name }} organisation."""
 
 
 def _append_footer(text, language, separator):
