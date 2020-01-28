03ac844532167ab5119279231f2cac493f16abc7
Riku Oja <riku.oja@kapsi.fi>
2020-01-28 17:44:43 +0200

    Add event publication status to admin

--- events/admin.py
+++ events/admin.py
@@ -38,7 +38,8 @@ class EventAdmin(AutoIdBaseAdmin, TranslationAdmin, VersionAdmin):
     # TODO: only allow user_editable editable fields
     fields = ('id', 'data_source', 'origin_id', 'name', 'short_description', 'description', 'location',
               'location_extra_info', 'start_time', 'end_time', 'keywords', 'audience', 'publisher', 'provider',
-              'provider_contact_info', 'event_status', 'super_event', 'info_url', 'in_language')
+              'provider_contact_info', 'event_status', 'super_event', 'info_url', 'in_language',
+              'publication_status')
     search_fields = ('name', 'location__name')
     list_display = ('id', 'name', 'start_time', 'end_time', 'publisher', 'location')
     list_filter = ('data_source',)
