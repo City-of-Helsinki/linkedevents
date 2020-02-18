7ae6189f79d8580a115a4819ae472500dc96ff52
Jari Pääkkö <jari.paakko@compile.fi>
2020-02-18 15:19:15 +0200

    chore(docker): fetch app password from SSM for batch jobs

--- entrypoint.importer.sh
+++ entrypoint.importer.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 # This file is used by Dockerfile.dist for running Linked Events importers
 
-set -euo pipefail
+set -eo pipefail
 
 # Require these env vars to be set
-: "${APP_PASSWORD:?}"
 : "${APP_USER:?}"
 : "${DB_HOST:?}"
 : "${DB_NAME:?}"
+
+# Makes it possible to fetch the app password from SSM instead of passing the password as an environment variable
+# because AWS Batch doesn't support passing secrets and displays all environment variables as plain text
+if [[ ! -z "$APP_PASSWORD_SSM_KEY" ]]; then
+  APP_PASSWORD=$( \
+    aws \
+      ssm get-parameter \
+      --name "$APP_PASSWORD_SSM_KEY" \
+      --query 'Parameter.Value' \
+      --with-decryption \
+      --region eu-west-1 | tr -d '\"'
+  )
+  unset APP_PASSWORD_SSM_KEY
+elif [[ -z "$APP_PASSWORD" ]]; then
+  echo "APP_PASSWORD not set"
+  exit 1
+fi
 
 APP_DATABASE_URL="postgis://${APP_USER}:${APP_PASSWORD}@${DB_HOST}/${DB_NAME}"
 unset APP_PASSWORD
