0a373eccba223fb87224d1b38bf56b9bb5d16c25
Ville Koivunen <ville.koivunen@hel.fi>
2020-02-19 21:16:55 +0200

    Only support linkedevents.settings in wsgi.py

--- linkedevents/wsgi.py
+++ linkedevents/wsgi.py
@@ -2,18 +2,12 @@
 WSGI config for linkedevents project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-if os.environ.get('PRODUCTION', None):
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "linkedevents.prod")
-elif os.environ.get('CD', None):
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "linkedevents.cd")
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "linkedevents.dev")
 
-from django.core.wsgi import get_wsgi_application  # noqa
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'linkedevents.settings')
+
 application = get_wsgi_application()
