dab1c8a83fa5db55f9dce55e6963e7aae7b3da74
Ville Koivunen <ville.koivunen@hel.fi>
2020-03-02 11:02:42 +0200

    Move entrypoint to shared command style with smbackend

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -23,11 +23,16 @@ if [ "$1" = "help" ]; then
 
     _log "You will need to mount a volume at /srv/media for storing user uploaded"
     _log "media. Otherwise they will be lost at container shutdown."
-
+    _log ""
     _log "In addition to the production server, there are several task commands:"
-    _log "runserver: runs Django development server at port 8000"
+    _log "start_django_development_server: runs Django development server at port 8000"
     _log "migrate: runs Django migrations (manage.py migrate)"
-
+    _log ""
+    _log "start_production_server: run production server (based on UWSGI) at port 8000"
+    _log "maintenance_tasks: run periodic maintenance tasks, with level as parameter"
+    _log ""
+    _log "e {anything}: run command in shell, verbatim"
+    _log ""
     _log_boxed "This container will now exit, for your convenience"
 
     exit 0
@@ -35,12 +40,15 @@ fi
 
 _log "Start with \`help\` for instructions"
 
-if [ "$1" = "runserver" ]; then
+if [ "$1" = "start_django_development_server" ]; then
     _log_boxed "Running development server"
     ./manage.py runserver 0:8000
-elif [ "$1" = "migrate" ]; then
+elif [ "$1" = "maintenance_tasks" ]; then
     _log_boxed "Running migrations"
     ./manage.py migrate
+elif [ "$1" = 'maintenance_tasks' ]; then
+    shift
+    deploy/run_imports.sh "$@"
 elif [ "$1" = "e" ]; then
     _log_boxed "exec'n $@"
     exec "$@"
