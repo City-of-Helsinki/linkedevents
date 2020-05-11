d87e640e1020cf409f49e3e1827de92b310c0141
Ville Koivunen <ville.koivunen@hel.fi>
2020-05-11 10:57:37 +0300

    Make maintenance runner chattier, to see that it really runs

--- deploy/run_maintenance.sh
+++ deploy/run_maintenance.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
 function run_all {
-    next=0
     echo "Running all maintenance tasks in /maintenance/stage_*.sh"
-    while [ -f /maintenance/stage_"$next".sh ]; do
-        echo "Found stage_$next"
-        /maintenance/stage_"$next".sh
+    for TASK_SCRIPT in /maintenance/stage_*.sh; do
+        echo "Found $TASK_SCRIPT, running"
+        $TASK_SCRIPT
         next=$((next+1))
     done
+    echo "All found maintenance tasks were run"
 }
 
+if ! ls /maintenance/stage_*.sh &> /dev/null; then
+    echo "No stage_*.sh maintenance scripts found in /maintenance"
+    echo "Either build such scripts into the image or mount them there."
+    echo "Exiting now"
+    exit 1
+fi
 
 if [ "$1" = 'all' ]; then
     run_all
 else
-    echo -n "Running maintenance tasks "
+    echo -n 'Running maintenance tasks '
     while test $# -gt 0
     do
         echo -n "$1"
         /maintenance/stage_"$1".sh
         shift
     done
+    echo .
+    echo 'Specified maintenance tasks run'
 fi
