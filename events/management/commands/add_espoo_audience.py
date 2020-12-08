89c24724e6316cd5a897101772f48e58ef0a42f4
Jari Pääkkö <jari.paakko@compile.fi>
2020-12-08 10:22:50 +0200

    refactor(mgmt): improve the naming of variables and functions
    - Rename the variables to better indicate that they only concern
      audience keywords
    - Prefix the functions with an underscore to indicate that they are
      non-public

--- events/management/commands/add_espoo_audience.py
+++ events/management/commands/add_espoo_audience.py
@@ -9,7 +9,7 @@ from events.models import Event, Keyword, KeywordSet, DataSource
 
 logger = logging.getLogger(__name__)
 
-ESPOO_KEYWORD_SET_DATA = {
+ESPOO_AUDIENCE_KEYWORD_SET_DATA = {
     'id': 'espoo:audiences',
     'name_en': 'Espoo\'s target audiences',
     'name_fi': 'Espoon kohderyhmät',
@@ -20,7 +20,7 @@ ESPOO_KEYWORD_SET_DATA = {
 
 # Note! If you update any of the keywords here, remember to also update KEYWORDS_TO_ADD_TO_AUDIENCE in
 # events/importer/yso.py
-KEYWORD_IDS = [
+AUDIENCE_KEYWORD_IDS = [
     'yso:p13050',  # lapsiperheet
     'yso:p16596',  # matkailijat
     'yso:p11617',  # nuoret
@@ -37,7 +37,7 @@ KEYWORD_IDS = [
 # wants but this has its own drawbacks since, e.g., some of the importers rely on the p2433 keyword. Therefore, we need
 # to make sure that any events that have the p2433 keyword also get updated with the custom Espoo keyword.
 # Audience keyword ID format: espoo:a<integer>, where a stands for audience followed by a sequential integer
-NEW_ESPOO_KEYWORDS = [
+CUSTOM_ESPOO_AUDIENCE_KEYWORDS = [
     {
         'id': 'espoo:a1',
         'name_fi': 'seniorit',
@@ -48,7 +48,7 @@ NEW_ESPOO_KEYWORDS = [
 ]
 
 # A mapping of YSO audience keywords to custom Espoo audience keywords
-YSO_TO_ESPOO_KEYWORD_MAPPING = {
+YSO_TO_ESPOO_AUDIENCE_KEYWORD_MAPPING = {
     'yso:p2433': 'espoo:a1',  # ikääntyneet -> seniorit
 }
 
@@ -57,12 +57,12 @@ class Command(BaseCommand):
     """Creates a keyword set with Espoo's audiences and maps YSO audience keywords to custom Espoo audience keywords.
 
     The mapping of YSO keywords to custom Espoo keywords is done so that any imported events that have any of the YSO
-    keywords specified in YSO_TO_ESPOO_KEYWORD_MAPPING are also augmented with the custom Espoo audience keywords. Of
-    course, the existing importers could be modified to instead directly add the custom Espoo keywords instead of using
-    this management command. However, then we'd need to modify multiple importers and the implementations of the
-    existing importers would diverge from the upstream linkedevents repository. This could be more fragile since any
-    future updates would need to take these changes into account. By making the update in this separate management
-    command, the changes are better isolated from existing functionality.
+    keywords specified in YSO_TO_ESPOO_AUDIENCE_KEYWORD_MAPPING are also augmented with the custom Espoo audience
+    keywords. Of course, the existing importers could be modified to instead directly add the custom Espoo keywords
+    instead of using this management command. However, then we'd need to modify multiple importers and the
+    implementations of the existing importers would diverge from the upstream linkedevents repository. This could be
+    more fragile since any future updates would need to take these changes into account. By making the update in this
+    separate management command, the changes are better isolated from existing functionality.
 
     Since some of the importers are run hourly, this management command should also be run hourly so that any imported
     events get augmented with the custom Espoo audience keywords.
@@ -73,7 +73,7 @@ class Command(BaseCommand):
     )
 
     @lru_cache()
-    def get_keyword_obj(self, keyword_id):
+    def _get_keyword_obj(self, keyword_id):
         try:
             keyword = Keyword.objects.get(id=keyword_id)
         except Keyword.DoesNotExist:
@@ -81,10 +81,10 @@ class Command(BaseCommand):
         return keyword
 
     @transaction.atomic()
-    def create_espoo_keywords(self):
-        logger.info('creating new Espoo keywords...')
+    def _create_espoo_audience_keywords(self):
+        logger.info('creating new Espoo audience keywords...')
 
-        for new_keyword in NEW_ESPOO_KEYWORDS:
+        for new_keyword in CUSTOM_ESPOO_AUDIENCE_KEYWORDS:
             keyword_set, created = Keyword.objects.update_or_create(
                 id=new_keyword['id'],
                 defaults=new_keyword
@@ -95,23 +95,23 @@ class Command(BaseCommand):
                 logger.info('keyword {} ({}) already exist'.format(new_keyword['name_fi'], new_keyword['id']))
 
     @transaction.atomic()
-    def create_espoo_audiences_keyword_set(self):
-        logger.info('creating Espoo target audiences keyword set...')
+    def _create_espoo_audiences_keyword_set(self):
+        logger.info('creating Espoo audiences keyword set...')
 
         # create the set itself
         keyword_set, created = KeywordSet.objects.update_or_create(
-            id=ESPOO_KEYWORD_SET_DATA['id'],
-            defaults=ESPOO_KEYWORD_SET_DATA
+            id=ESPOO_AUDIENCE_KEYWORD_SET_DATA['id'],
+            defaults=ESPOO_AUDIENCE_KEYWORD_SET_DATA
         )
         if created:
-            logger.info('created keyword set "{}"'.format(ESPOO_KEYWORD_SET_DATA['id']))
+            logger.info('created keyword set "{}"'.format(ESPOO_AUDIENCE_KEYWORD_SET_DATA['id']))
         else:
-            logger.info('keyword set "{}" already exist'.format(ESPOO_KEYWORD_SET_DATA['id']))
+            logger.info('keyword set "{}" already exist'.format(ESPOO_AUDIENCE_KEYWORD_SET_DATA['id']))
 
         # add the keywords to the set
         existing_keywords = set(keyword_set.keywords.all())
-        for keyword_id in KEYWORD_IDS:
-            keyword = self.get_keyword_obj(keyword_id)
+        for keyword_id in AUDIENCE_KEYWORD_IDS:
+            keyword = self._get_keyword_obj(keyword_id)
 
             if keyword not in existing_keywords:
                 keyword_set.keywords.add(keyword)
@@ -119,18 +119,18 @@ class Command(BaseCommand):
                 logger.info('added {} ({}) to the keyword set'.format(keyword.name, keyword_id))
 
     @transaction.atomic()
-    def add_espoo_audience_keywords_to_events(self):
+    def _add_espoo_audience_keywords_to_events(self):
         logger.info('Adding Espoo audience keywords to events...')
 
         for event in Event.objects.exclude(audience__isnull=True).prefetch_related('audience'):
             for audience in event.audience.all():
 
-                if audience.id not in YSO_TO_ESPOO_KEYWORD_MAPPING:
+                if audience.id not in YSO_TO_ESPOO_AUDIENCE_KEYWORD_MAPPING:
                     continue
 
                 # Map the given YSO audience keyword to a custom Espoo audience keyword
-                espoo_keyword_id = YSO_TO_ESPOO_KEYWORD_MAPPING.get(audience.id)
-                espoo_keyword_obj = self.get_keyword_obj(espoo_keyword_id)
+                espoo_keyword_id = YSO_TO_ESPOO_AUDIENCE_KEYWORD_MAPPING.get(audience.id)
+                espoo_keyword_obj = self._get_keyword_obj(espoo_keyword_id)
 
                 if espoo_keyword_obj not in event.audience.all():
                     event.audience.add(espoo_keyword_obj)
@@ -140,9 +140,9 @@ class Command(BaseCommand):
         # Espoo data source must be created if missing. Note that it is not necessarily the system data source.
         # If we are creating it, it *may* still be the system data source, so it must be user editable!
         espoo_data_source_defaults = {'user_editable': True}
-        DataSource.objects.get_or_create(id=ESPOO_KEYWORD_SET_DATA['data_source_id'],
+        DataSource.objects.get_or_create(id=ESPOO_AUDIENCE_KEYWORD_SET_DATA['data_source_id'],
                                          defaults=espoo_data_source_defaults)
-        self.create_espoo_keywords()
-        self.create_espoo_audiences_keyword_set()
-        self.add_espoo_audience_keywords_to_events()
+        self._create_espoo_audience_keywords()
+        self._create_espoo_audiences_keyword_set()
+        self._add_espoo_audience_keywords_to_events()
         logger.info('all done')
