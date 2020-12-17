7f12ad9e14d58557229c0a1e424f09ab6ab2c45d
Jari Pääkkö <jari.paakko@compile.fi>
2020-12-17 14:27:44 +0200

    feat(docker): specify the ongoing_local cache URL for the admin image
    Specify the ONGOING_LOCAL_CACHE_URL environment variable for the
    admin Docker image used for running management commands.
    
    Also, make it possible to define whether a TLS connection should be used
    for the Redis connection using the CACHE_TLS environment variable.

--- entrypoint.admin.sh
+++ entrypoint.admin.sh
@@ -8,9 +8,26 @@ set -eo pipefail
 : "${DB_HOST:?}"
 : "${DB_NAME:?}"
 
-# Makes it possible to fetch the app password from SSM instead of passing the password as an environment variable
+CACHE_TLS="${CACHE_TLS:-true}"
+ONGOING_LOCAL_CACHE_DB="${ONGOING_LOCAL_CACHE_DB:-"1"}"
+
+# Makes it possible to fetch the app passwords from SSM instead of passing the passwords as environment variables
 # because AWS Batch doesn't support passing secrets and displays all environment variables as plain text
-if [[ ! -z "$DB_APP_PASSWORD_SSM_KEY" ]]; then
+if [[ -n "$CACHE_PASSWORD_SSM_KEY" ]]; then
+  CACHE_PASSWORD=$( \
+    aws \
+      ssm get-parameter \
+      --name "$CACHE_PASSWORD_SSM_KEY" \
+      --query 'Parameter.Value' \
+      --with-decryption \
+      --region eu-west-1 | tr -d '\"'
+  )
+  unset CACHE_PASSWORD_SSM_KEY
+elif [[ -z "$CACHE_PASSWORD" ]]; then
+  echo "CACHE_PASSWORD not set"
+fi
+
+if [[ -n "$DB_APP_PASSWORD_SSM_KEY" ]]; then
   DB_APP_PASSWORD=$( \
     aws \
       ssm get-parameter \
@@ -25,6 +42,22 @@ elif [[ -z "$DB_APP_PASSWORD" ]]; then
   exit 1
 fi
 
+# The ONGOING_LOCAL_CACHE_URL is currently only used by the populate_local_event_cache management command so that's why
+# it's optional and only set if CACHE_PASSWORD and CACHE_HOST are set
+if [[ -n "$CACHE_PASSWORD" && -n "$CACHE_HOST" ]]; then
+
+  # Set the correct URL scheme for the cache connection based on the CACHE_TLS environment variable. Redis doesn't
+  # provide TSL by default and the local Redis container doesn't have any extra configuration for setting up TSL. Since,
+  # the admin Docker image is used both in the local and non-local environments, we need to be able to control the TLS
+  # setting accordingly. Thus, this setting is mostly needed for using a non-TLS connection in the local environment.
+  if [ "$CACHE_TLS" = true ]; then
+    CACHE_URL_SCHEME="rediss"
+  else
+    CACHE_URL_SCHEME="redis"
+  fi
+
+  export ONGOING_LOCAL_CACHE_URL="${CACHE_URL_SCHEME}://:${CACHE_PASSWORD}@${CACHE_HOST}/${ONGOING_LOCAL_CACHE_DB}"
+fi
 export DATABASE_URL="postgis://${DB_APP_USER}:${DB_APP_PASSWORD}@${DB_HOST}/${DB_NAME}"
 
 # Run as exec so the application can receive any Unix signals sent to the container, e.g., Ctrl + C.
