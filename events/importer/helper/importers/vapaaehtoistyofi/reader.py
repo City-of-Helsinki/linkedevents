6bd3a2842dbdadbe7f6ad2773834d980b0402fbd
Jari Turkia <jari.turkia@capgemini.com>
2021-05-07 15:42:56 +0300

    Started using new endpoint for single tasks. Added proper session support into HTTP-client.

--- events/importer/helper/importers/vapaaehtoistyofi/reader.py
+++ events/importer/helper/importers/vapaaehtoistyofi/reader.py
@@ -9,19 +9,12 @@ class Reader:
     endpoint_url = 'https://apiv2.vapaaehtoistyo.fi'
     rest_user_agent = 'HelsinkiVETImporter/0.1'
     timeout = 5.0
-    cached_entries = True
 
     def __init__(self, api_key):
         if not api_key:
             raise ValueError("Really need API-key!")
         self.api_key = api_key
-
-        if self.cached_entries:
-            cnt_entries, data = self.load_entries()
-            self.entries = {}
-            for record in data:
-                id = record.id
-                self.entries[id] = record
+        self.http_client = None
 
     def _setup_client(self):
         headers = {
@@ -35,50 +28,48 @@ class Reader:
 
         return s
 
-    def load_entry(self, id):
-        if not self.cached_entries:
-            return self._load_entry_api(id)
+    def _get_client(self):
+        if self.http_client:
+            return self.http_client
 
-        if id not in self.entries:
-            return False
+        self.http_client = self._setup_client()
 
-        return self.entries[id]
+        return self.http_client
 
-    def _load_entry_api(self, id):
-        http_client = self._setup_client()
-        url = "%s/task/%s" % (self.endpoint_url, id)
-        response = http_client.get(url, timeout=self.timeout)
+    @staticmethod
+    def _load_status_check(data):
+        if 'status' not in data or data['status'] != "ok":
+            raise RuntimeError("Vapaaehtoistyö.fi response isn't ok!")
+        if 'data' not in data:
+            raise RuntimeError("Vapaaehtoistyö.fi response doesn't contain 'data'!")
+
+    def load_entry(self, event_id):
+        url = "%s/task/%s" % (self.endpoint_url, event_id)
+        response = self._get_client().get(url, timeout=self.timeout)
         if response.status_code != 200:
             raise RuntimeError("Failed to request data from Vapaaehtoistyö.fi API! HTTP/%d" %
                                response.status_code)
 
         data = response.json()
-        if 'status' not in data or data['status'] != "ok":
-            raise RuntimeError("Vapaaehtoistyö.fi response isn't ok!")
-        if 'data' not in data:
-            raise RuntimeError("Vapaaehtoistyö.fi response doesn't contain 'data'!")
+        self._load_status_check(data)
         data_obj = Record(data)
 
         return data_obj
 
     def load_entries(self):
-        http_client = self._setup_client()
         page = 1
         total_records = None
         batch_size = 1
         ret = []
         while batch_size:
             url = "%s/collection/task?page=%d" % (self.endpoint_url, page)
-            response = http_client.get(url, timeout=self.timeout)
+            response = self._get_client().get(url, timeout=self.timeout)
 
             if response.status_code != 200:
                 raise RuntimeError("Failed to request data from Vapaaehtoistyö.fi API! HTTP/%d" %
                                    response.status_code)
             data = response.json()
-            if 'status' not in data or data['status'] != "ok":
-                raise RuntimeError("Vapaaehtoistyö.fi response isn't ok!")
-            if 'data' not in data:
-                raise RuntimeError("Vapaaehtoistyö.fi response doesn't contain 'data'!")
+            self._load_status_check(data)
             if not total_records:
                 total_records = int(data['data']['totalRecords'])
 
@@ -106,8 +97,6 @@ class Reader:
         if 'status' not in data or data['status'] != "ok":
             raise RuntimeError("Vapaaehtoistyö.fi response isn't ok!")
         if 'data' not in data:
-            # log.error("%s: %s" % (id, data))
-            # raise RuntimeError("Vapaaehtoistyö.fi response doesn't contain 'data'!"
             log.warning("Requested photo for %s, but didn't receive any data!" % id)
             return None, None
         if data['data']['photo']['type'] != "Buffer":
