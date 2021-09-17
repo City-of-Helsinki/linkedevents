e03dc809698326cc8353db278befbc244811dac7
Anthon <60883871+Anthon98@users.noreply.github.com>
2021-09-17 13:16:57 +0300

    Added math to calculate the max page count in palvelukartta api within tprapi async module due to data being mutable (#50)
    Made changes to the tprapi importer module code to fix a problem due to page count not being immutable in palvelukartta. Now the page count is calculated appropriately according to the default 20 value from servicemap. This is done through a singular synchronous call and required the appropriate library addition as well as the math library.

--- events/importer/tprapi.py
+++ events/importer/tprapi.py
@@ -1,6 +1,8 @@
+import requests
 import asyncio
 import aiohttp
 import logging
+import math
 import json
 import sys
 
@@ -14,7 +16,7 @@ headers = {
 }
 
 
-async def fetch(session, url, id):
+async def fetch(session, url, id) -> dict:
     async with session.get(url) as response:
         data = await response.read()
         json_response = json.loads(data)
@@ -31,8 +33,9 @@ async def fetch(session, url, id):
                 "Palvelukartta unit: '%s' request did not fail, but API parsing was incorrect. API has changed?" % id)
 
 
-async def main(unit_pages=63):
+async def main(unit_pages) -> dict:
     async with aiohttp.ClientSession() as session:
+
         tasks = []
         for page_no in range(1, unit_pages):
             URL = 'https://palvelukartta.turku.fi/api/v2/unit/?page=%s' % page_no
@@ -45,18 +48,36 @@ async def main(unit_pages=63):
             await asyncio.sleep(1)
 
 
-def async_main(retry_count=3):
+def async_main(retry_count=3) -> dict:
     # Currently works in 3.6 Python
     # Works for both 3.6 and 3.7 Python versions.
+    
+    # Fetch page_count by calculating page_size 'count'
+    # by dividing it with default value 20 (servicemap).
+    url = "https://palvelukartta.turku.fi/api/v2/unit"
+
+    try:
+        fetch = requests.get(url, headers=headers)
+        response = fetch.json()
+        calc = math.modf(response['count'] / 20)
+        
+        if calc[0] != float(0):
+            page_count = int(calc[1]+1)
+        else:
+            page_count = int(calc[1])
+    except Exception as e:
+         raise Exception(
+            "Something went wrong in the 'page count' pre-processing phase of TPRAPI: %s" % e)
+
     for connection_retries in range(retry_count):
         if sys.version_info >= (3, 8, 0):
-            res = asyncio.run(main())
+            res = asyncio.run(main(unit_pages=page_count))
             if res:
                 return res
         else:
             # 3.6+ support.
             loop = asyncio.get_event_loop()
-            task = loop.create_task(main())
+            task = loop.create_task(main(unit_pages=page_count))
             a = loop.run_until_complete(task)
             task.cancel()
             if a:
