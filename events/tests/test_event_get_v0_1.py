f4a60dd86bd0306a7c3dc9960a96ffe2f3dcdd26
Pasi Huhtiniemi <pasi.huhtiniemi@reaktor.com>
2022-12-28 11:24:50 +0200

    chore: fix lint errors

--- events/tests/test_event_get_v0_1.py
+++ events/tests/test_event_get_v0_1.py
@@ -12,7 +12,7 @@ def test__get_event_list_check_fields_exist(api_client, event):
     Tests that event list endpoint returns the image as null.
     """
     response = get_list(api_client, version='v0.1')
-    assert(not response.data['data'][0]['image'])
+    assert (not response.data['data'][0]['image'])
 
 
 @pytest.mark.django_db
@@ -21,7 +21,7 @@ def test__get_event_detail_check_fields_exist(api_client, event):
     Tests that event detail endpoint returns the image as null.
     """
     response = get_detail(api_client, event.pk, version='v0.1')
-    assert(not response.data['image'])
+    assert (not response.data['image'])
 
 
 @pytest.mark.django_db
@@ -33,4 +33,4 @@ def test__api_get_event_list_check_fields_exist(api_get_list):
     This one sets version on module level and does not deal with API client
     """
     response = api_get_list()
-    assert(not response.data['data'][0]['image'])
+    assert (not response.data['data'][0]['image'])
