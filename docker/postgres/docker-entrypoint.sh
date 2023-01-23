475ce870212b4893b65164c6da1a656ff35829d8
Juha Louhiranta <juha.louhiranta@anders.fi>
2023-01-23 11:39:22 +0200

    Add hstore and pg_trgm to docker postgres default DB template
    This will allow running pytest with --no-migrations flag
    since by default postgres uses the template1 DB as the template.
    
    Also fixes an issue where the baseimage had renamed entrypoint
    file.
    
    Refs: https://github.com/City-of-Helsinki/docker-images/pull/18

--- docker/postgres/docker-entrypoint.sh
+++ docker/postgres/docker-entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 #################################################
 # This file is taken directly from:
 # https://github.com/City-of-Helsinki/docker-images/blob/master/templates/postgis/9.6-2.5/initdb-postgis.sh
-# Only "hstore" extension has been added
+# Only "hstore" and "pg_trgm" extensions have been added.
 #################################################
 
 set -e
@@ -13,17 +13,20 @@ export PGUSER="$POSTGRES_USER"
 
 # Create the 'template_postgis' template db
 "${psql[@]}" <<- 'EOSQL'
-CREATE DATABASE template_postgis;
-UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
+CREATE DATABASE template_postgis IS_TEMPLATE true;
 EOSQL
 
 # Load PostGIS into both template_database and $POSTGRES_DB
-for DB in template_postgis "$POSTGRES_DB"; do
+for DB in template1 template_postgis "$POSTGRES_DB"; do
 	echo "Loading PostGIS extensions into $DB"
 	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
 		CREATE EXTENSION IF NOT EXISTS postgis;
 		CREATE EXTENSION IF NOT EXISTS hstore;
+		CREATE EXTENSION IF NOT EXISTS pg_trgm;
 		CREATE EXTENSION IF NOT EXISTS postgis_topology;
+		-- Reconnect to update pg_setting.resetval
+		-- See https://github.com/postgis/docker-postgis/issues/288
+		\c
 		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
 		CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
 EOSQL
