b4ba6f5b2909c497c88436fa83eccdaeb7560a8b
Juha Louhiranta <juha.louhiranta@anders.fi>
2023-02-03 15:45:36 +0200

    Use receiver decorator on event.signals
    Refs LINK-1137

--- events/apps.py
+++ events/apps.py
@@ -1,22 +1,8 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_save
 
 
 class EventsConfig(AppConfig):
     name = "events"
 
     def ready(self):
-        from django.contrib.auth import get_user_model
-
-        from .signals import organization_post_save, user_post_save
-
-        post_save.connect(
-            organization_post_save,
-            sender="django_orghierarchy.Organization",
-            dispatch_uid="organization_post_save",
-        )
-        post_save.connect(
-            user_post_save,
-            sender=get_user_model(),
-            dispatch_uid="user_post_save",
-        )
+        import events.signals  # noqa
