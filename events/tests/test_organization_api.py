e28447789e8f99dad0823b7496a6f3538b8a5484
Jori Lindell <jori.lindell@gmail.com>
2023-06-30 07:05:38 +0300

    Use user_api_client fixture instead of api_client in organization tests

--- events/tests/test_organization_api.py
+++ events/tests/test_organization_api.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from events.api import OrganizationDetailSerializer
 from events.tests.utils import versioned_reverse as reverse
 
+organization_name = "test org"
+edited_organization_name = "new name"
 
 def organization_id(pk):
     obj_id = reverse(OrganizationDetailSerializer.view_name, kwargs={"pk": pk})
@@ -59,11 +61,10 @@ def assert_update_organization(api_client, pk, organization_data):
 
 
 @pytest.mark.django_db
-def test_admin_user_can_see_organization_users(api_client, organization, user):
+def test_admin_user_can_see_organization_users(organization, user, user_api_client):
     organization.regular_users.add(user)
-    api_client.force_authenticate(user)
 
-    response = get_organization(api_client, organization.id)
+    response = get_organization(user_api_client, organization.id)
     assert response.data["admin_users"]
     assert response.data["regular_users"]
 
@@ -78,68 +79,61 @@ def test_anonymous_user_cannot_see_organization_users(api_client, organization, 
 
 
 @pytest.mark.django_db
-def test_regular_user_cannot_see_organization_users(api_client, organization, user):
+def test_regular_user_cannot_see_organization_users(organization, user, user_api_client):
     organization.regular_users.add(user)
     organization.admin_users.remove(user)
-    api_client.force_authenticate(user)
 
-    response = get_organization(api_client, organization.id)
+    response = get_organization(user_api_client, organization.id)
     assert response.data.get("admin_users") == None
     assert response.data.get("regular_users") == None
 
 
 @pytest.mark.django_db
 def test_admin_user_can_create_organization(
-    api_client, data_source, organization, user
+    data_source, organization, user_api_client
 ):
-    api_client.force_authenticate(user)
-
     origin_id = "test_organization2"
     payload = {
         "data_source": data_source.id,
         "origin_id": origin_id,
         "id": f"{data_source.id}:{origin_id}",
-        "name": "test org",
+        "name": organization_name,
     }
 
-    response = assert_create_organization(api_client, payload)
+    response = assert_create_organization(user_api_client, payload)
     assert response.data["name"] == payload["name"]
 
 
 @pytest.mark.django_db
 def test_cannot_create_organization_with_existing_id(
-    api_client, data_source, organization, user
+    organization, user_api_client
 ):
-    api_client.force_authenticate(user)
-
     payload = {
         "data_source": organization.data_source.id,
         "origin_id": organization.origin_id,
         "id": organization.id,
-        "name": "test org",
+        "name": organization_name,
     }
 
-    response = create_organization(api_client, payload)
+    response = create_organization(user_api_client, payload)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.data["non_field_errors"][0].code == "unique"
 
 
 @pytest.mark.django_db
 def test_admin_user_can_create_organization_with_parent(
-    api_client, data_source, organization, user
+    data_source, organization, user_api_client
 ):
-    api_client.force_authenticate(user)
-
     origin_id = "test_organization2"
     payload = {
         "data_source": data_source.id,
         "origin_id": origin_id,
         "id": f"{data_source.id}:{origin_id}",
-        "name": "test org",
+        "name": organization_name,
         "parent_organization": organization_id(organization.pk),
     }
 
-    response = assert_create_organization(api_client, payload)
+    response = assert_create_organization(user_api_client, payload)
     assert response.data["parent_organization"] == payload["parent_organization"]
 
 
@@ -154,7 +148,7 @@ def test_cannot_create_organization_with_parent_user_has_no_rights(
         "data_source": data_source.id,
         "origin_id": origin_id,
         "id": f"{data_source.id}:{origin_id}",
-        "name": "test org",
+        "name": organization_name,
         "parent_organization": organization_id(organization.pk),
     }
 
@@ -165,46 +159,42 @@ def test_cannot_create_organization_with_parent_user_has_no_rights(
 
 @pytest.mark.django_db
 def test_create_organization_with_sub_organizations(
-    api_client, data_source, organization, organization2, user
+    data_source, organization, organization2, user_api_client
 ):
-    api_client.force_authenticate(user)
-
     origin_id = "test_organization2"
     payload = {
         "data_source": data_source.id,
         "origin_id": origin_id,
         "id": f"{data_source.id}:{origin_id}",
-        "name": "test org",
+        "name": organization_name,
         "sub_organizations": [
             organization_id(organization.id),
             organization_id(organization2.id),
         ],
     }
 
-    response = assert_create_organization(api_client, payload)
+    response = assert_create_organization(user_api_client, payload)
     org_id = response.data["id"]
-    response = get_organization(api_client, org_id)
+    response = get_organization(user_api_client, org_id)
     assert set(response.data["sub_organizations"]) == set(payload["sub_organizations"])
 
 
 @pytest.mark.django_db
 def test_cannot_add_sub_organization_with_wrong_id(
-    api_client, data_source, organization, organization2, user
+    data_source, organization, organization2, user_api_client
 ):
-    api_client.force_authenticate(user)
-
     origin_id = "test_organization2"
     payload = {
         "data_source": data_source.id,
         "origin_id": origin_id,
         "id": f"{data_source.id}:{origin_id}",
-        "name": "test org",
+        "name": organization_name,
         "sub_organizations": ["wrong.id", organization_id(organization2.id)],
     }
 
-    response = assert_create_organization(api_client, payload)
+    response = assert_create_organization(user_api_client, payload)
     org_id = response.data["id"]
-    response = get_organization(api_client, org_id)
+    response = get_organization(user_api_client, org_id)
     assert set(response.data["sub_organizations"]) == set(
         [organization_id(organization2.id)]
     )
@@ -212,16 +202,14 @@ def test_cannot_add_sub_organization_with_wrong_id(
 
 @pytest.mark.django_db
 def test_create_organization_with_affiliated_organizations(
-    api_client, data_source, organization, organization2, user
+    data_source, organization, organization2, user_api_client
 ):
-    api_client.force_authenticate(user)
-
     origin_id = "test_organization2"
     payload = {
         "data_source": data_source.id,
         "origin_id": origin_id,
         "id": f"{data_source.id}:{origin_id}",
-        "name": "test org",
+        "name": organization_name,
         "affiliated_organizations": [
             organization_id(organization.id),
             organization_id(organization2.id),
@@ -232,45 +220,41 @@ def test_create_organization_with_affiliated_organizations(
         i.internal_type = Organization.AFFILIATED
         i.save()
 
-    response = assert_create_organization(api_client, payload)
+    response = assert_create_organization(user_api_client, payload)
     org_id = response.data["id"]
-    response = get_organization(api_client, org_id)
+    response = get_organization(user_api_client, org_id)
     assert set(response.data["affiliated_organizations"]) == set(
         payload["affiliated_organizations"]
     )
 
 
 @pytest.mark.django_db
-def test_user_is_automatically_added_to_admins_users(api_client, data_source, user):
-    api_client.force_authenticate(user)
-
+def test_user_is_automatically_added_to_admins_users(data_source, user, user_api_client):
     origin_id = "test_organization2"
     payload = {
         "data_source": data_source.id,
         "origin_id": origin_id,
         "id": f"{data_source.id}:{origin_id}",
-        "name": "test org",
+        "name": organization_name,
     }
 
-    response = assert_create_organization(api_client, payload)
+    response = assert_create_organization(user_api_client, payload)
     assert response.data["admin_users"][0]["username"] == user.username
 
 
 @pytest.mark.django_db
-def test_admin_user_add_users_to_new_organization(api_client, data_source, user, user2):
-    api_client.force_authenticate(user)
-
+def test_admin_user_add_users_to_new_organization(data_source, user, user2, user_api_client):
     origin_id = "test_organization2"
     payload = {
         "data_source": data_source.id,
         "origin_id": origin_id,
         "id": f"{data_source.id}:{origin_id}",
-        "name": "test org",
+        "name": organization_name,
         "admin_users": [user.username, user2.username],
         "regular_users": [user2.username],
     }
 
-    response = assert_create_organization(api_client, payload)
+    response = assert_create_organization(user_api_client, payload)
     assert set(payload["admin_users"]) == set(
         [i["username"] for i in response.data["admin_users"]]
     )
@@ -280,15 +264,13 @@ def test_admin_user_add_users_to_new_organization(api_client, data_source, user,
 
 
 @pytest.mark.django_db
-def test_admin_user_can_update_organization(api_client, organization, user):
-    api_client.force_authenticate(user)
-
+def test_admin_user_can_update_organization(organization, user_api_client):
     payload = {
         "id": organization.id,
-        "name": "new name",
+        "name": edited_organization_name,
     }
 
-    response = assert_update_organization(api_client, organization.id, payload)
+    response = assert_update_organization(user_api_client, organization.id, payload)
     assert response.data["name"] == payload["name"]
 
 
@@ -296,7 +278,7 @@ def test_admin_user_can_update_organization(api_client, organization, user):
 def test_anonymous_user_cannot_update_organization(api_client, organization):
     payload = {
         "id": organization.id,
-        "name": "new name",
+        "name": edited_organization_name,
     }
 
     response = update_organization(api_client, organization.id, payload)
@@ -304,17 +286,16 @@ def test_anonymous_user_cannot_update_organization(api_client, organization):
 
 
 @pytest.mark.django_db
-def test_regular_user_cannot_update_organization(api_client, organization, user):
+def test_regular_user_cannot_update_organization(organization, user, user_api_client):
     user.get_default_organization().regular_users.add(user)
     user.get_default_organization().admin_users.remove(user)
-    api_client.force_authenticate(user)
-
+    
     payload = {
         "id": organization.id,
-        "name": "new name",
+        "name": edited_organization_name,
     }
 
-    response = update_organization(api_client, organization.id, payload)
+    response = update_organization(user_api_client, organization.id, payload)
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
@@ -326,7 +307,7 @@ def test_admin_user_from_other_organization_cannot_update_organization(
 
     payload = {
         "id": organization.id,
-        "name": "new name",
+        "name": edited_organization_name,
     }
 
     response = update_organization(api_client, organization.id, payload)
@@ -334,8 +315,7 @@ def test_admin_user_from_other_organization_cannot_update_organization(
 
 
 @pytest.mark.django_db
-def test_admin_user_can_edit_users(api_client, organization, super_user, user, user2):
-    api_client.force_authenticate(user)
+def test_admin_user_can_edit_users(organization, super_user, user, user2, user_api_client):
     organization.admin_users.add(super_user)
 
     payload = {
@@ -345,7 +325,7 @@ def test_admin_user_can_edit_users(api_client, organization, super_user, user, u
         "regular_users": [user2.username],
     }
 
-    response = assert_update_organization(api_client, organization.pk, payload)
+    response = assert_update_organization(user_api_client, organization.pk, payload)
     assert set(payload["admin_users"]) == set(
         [i["username"] for i in response.data["admin_users"]]
     )
@@ -355,21 +335,17 @@ def test_admin_user_can_edit_users(api_client, organization, super_user, user, u
 
 
 @pytest.mark.django_db
-def test_user_cannot_remove_itself_from_admins(user, organization, api_client):
-    api_client.force_authenticate(user)
-
+def test_user_cannot_remove_itself_from_admins(organization, user, user_api_client):
     payload = {"id": organization.id, "name": organization.name, "admin_users": []}
 
-    response = assert_update_organization(api_client, organization.pk, payload)
+    response = assert_update_organization(user_api_client, organization.pk, payload)
     assert response.data["admin_users"][0]["username"] == user.username
 
 
 @pytest.mark.django_db
-def test_admin_user_can_delete_organization(api_client, organization, user):
-    api_client.force_authenticate(user)
-
-    assert_delete_organization(api_client, organization.id)
-    response = get_organization(api_client, organization.id)
+def test_admin_user_can_delete_organization(organization, user_api_client):
+    assert_delete_organization(user_api_client, organization.id)
+    response = get_organization(user_api_client, organization.id)
     assert response.status_code == 404
 
 
@@ -380,12 +356,11 @@ def test_anonymous_user_can_delete_organization(api_client, organization):
 
 
 @pytest.mark.django_db
-def test_regular_user_cannot_delete_organization(api_client, organization, user):
+def test_regular_user_cannot_delete_organization(organization, user, user_api_client):
     user.get_default_organization().regular_users.add(user)
     user.get_default_organization().admin_users.remove(user)
-    api_client.force_authenticate(user)
 
-    response = delete_organization(api_client, organization.id)
+    response = delete_organization(user_api_client, organization.id)
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
