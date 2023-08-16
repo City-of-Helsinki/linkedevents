61757ecc0fa7bf513b62f29e4ec640cc5921cfb3
Harri Rissanen <harri.rissanen@vincit.fi>
2023-08-16 13:06:02 +0300

    Update existing helevents.tests for registration admins

--- helevents/tests/test_models.py
+++ helevents/tests/test_models.py
@@ -68,6 +68,25 @@ class TestUser(TestCase):
         is_admin = self.user.is_admin_of(self.org_1)
         self.assertFalse(is_admin)
 
+    def test_is_registration_admin(self):
+        self.org.registration_admin_users.add(self.user)
+
+        # test for admin organization
+        is_registration_admin = self.user.is_registration_admin_of(self.org)
+        self.assertTrue(is_registration_admin)
+
+        # test for child organization
+        is_registration_admin = self.user.is_registration_admin_of(self.child_org)
+        self.assertTrue(is_registration_admin)
+
+        # test for affiliated organization
+        is_registration_admin = self.user.is_registration_admin_of(self.child_org)
+        self.assertTrue(is_registration_admin)
+
+        # test for some other organization
+        is_registration_admin = self.user.is_registration_admin_of(self.org_1)
+        self.assertFalse(is_registration_admin)
+
     def test_is_regular_user(self):
         self.org.regular_users.add(self.user)
 
