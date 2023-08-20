City-of-Helsinki/linkedevents

commit 8ac8ec78c34aba7ee90159f64b7797a2a5dadd91

Hide data source, organization class and feedback endpoints from the api root view

linkedevents/api.py
@@ -1,12 +1,49 @@
 import copy
+from collections import OrderedDict
 
 from django.conf import settings
-from rest_framework.routers import DefaultRouter, SimpleRouter
+from django.urls import NoReverseMatch
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.routers import APIRootView, DefaultRouter, SimpleRouter
 
 from linkedevents.registry import all_views
 
 
+class CustomAPIRootView(APIRootView):
+    def get(self, request, *args, **kwargs):
+        # Return a plain {"name": "hyperlink"} response.
+        ret = OrderedDict()
+        namespace = request.resolver_match.namespace
+        for key, url_name in self.api_root_dict.items():
+            # Don't show data source, feedback and organization class routes in the api root
+            if url_name in [
+                "datasource-list",
+                "organizationclass-list",
+                "feedback-list",
+                "guest-feedback-list",
+            ]:
+                continue
+
+            if namespace:
+                url_name = namespace + ":" + url_name
+            try:
+                ret[key] = reverse(
+                    url_name,
+                    args=args,
+                    kwargs=kwargs,
+                    request=request,
+                    format=kwargs.get("format"),
+                )
+            except NoReverseMatch:
+                # Don't bail out if eg. no list routes exist, only detail routes.
+                continue
+
+        return Response(ret)
+
+
 class LinkedEventsAPIRouter(DefaultRouter):
+    APIRootView = CustomAPIRootView
     # these are from Django REST Framework bulk BulkRouter with 'delete' excluded
     routes = copy.deepcopy(SimpleRouter.routes)
     routes[0].mapping.update(

linkedevents/tests/test_api_root_view.py
@@ -1,0 +1,29 @@
+import pytest
+
+from events.tests.utils import versioned_reverse as reverse
+
+
+def get_api_root(api_client):
+    url = reverse("api-root")
+
+    return api_client.get(url)
+
+
+@pytest.mark.django_db
+def test_return_correct_routes(api_client):
+    response = get_api_root(api_client)
+    assert response.data["keyword"] == "http://testserver/v1/keyword/"
+    assert response.data["keyword_set"] == "http://testserver/v1/keyword_set/"
+    assert response.data["place"] == "http://testserver/v1/place/"
+    assert response.data["language"] == "http://testserver/v1/language/"
+    assert response.data["organization"] == "http://testserver/v1/organization/"
+    assert response.data["image"] == "http://testserver/v1/image/"
+    assert response.data["event"] == "http://testserver/v1/event/"
+    assert response.data["search"] == "http://testserver/v1/search/"
+    assert response.data["user"] == "http://testserver/v1/user/"
+    assert response.data["registration"] == "http://testserver/v1/registration/"
+    assert response.data["signup"] == "http://testserver/v1/signup/"
+    assert response.data.get("data_source") == None
+    assert response.data.get("organization_class") == None
+    assert response.data.get("feedback") == None
+    assert response.data.get("guest-feedback") == None

commit a83c32869d9d2d51c92934837adfd603bba01914

Fix API root view name
Fixed API root view name which was erroneous "Custom Api Root" due to a
regression introduced in 8ac8ec78c34aba7ee90159f64b7797a2a5dadd91.

linkedevents/api.py
@@ -11,6 +11,8 @@ from linkedevents.registry import all_views
 
 
 class CustomAPIRootView(APIRootView):
+    name = "Linked Events"
+
     def get(self, request, *args, **kwargs):
         # Return a plain {"name": "hyperlink"} response.
         ret = OrderedDict()

linkedevents/tests/test_api_root_view.py
@@ -31,3 +31,8 @@ def test_return_correct_routes(api_client):
     assert response.data.get("organization_class") == None
     assert response.data.get("feedback") == None
     assert response.data.get("guest-feedback") == None
+
+
+def test_has_correct_name(api_client):
+    response = api_client.get(reverse("api-root"), HTTP_ACCEPT="text/html")
+    assert "<h1>Linked Events</h1>" in str(response.content)