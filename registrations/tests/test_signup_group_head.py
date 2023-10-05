55d7e67ca55f3268913a001fcf0f2c4a9ccb9b8f
Harri Rissanen <harri.rissanen@vincit.fi>
2023-10-05 15:19:46 +0300

    Add tests for HEAD method not allowed
    LINK-1587

---
+++ registrations/tests/test_signup_group_head.py
@@ -0,0 +1,42 @@
+from typing import Union
+
+import pytest
+from rest_framework import status
+
+from events.tests.conftest import APIClient
+from events.tests.utils import versioned_reverse as reverse
+from helevents.tests.factories import UserFactory
+from registrations.tests.factories import SignUpGroupFactory
+
+# === util methods ===
+
+
+def head_list(api_client: APIClient):
+    url = reverse("signupgroup-list")
+
+    return api_client.head(url)
+
+
+def head_detail(api_client: APIClient, pk: Union[str, int]):
+    url = reverse("signupgroup-detail", kwargs={"pk": pk})
+
+    return api_client.head(url)
+
+
+# === tests ===
+
+
+@pytest.mark.parametrize("url_type", ["list", "detail"])
+@pytest.mark.django_db
+def test_head_method_not_allowed_for_signup_group(api_client, registration, url_type):
+    user = UserFactory()
+    user.registration_admin_organizations.add(registration.publisher)
+    api_client.force_authenticate(user)
+
+    if url_type == "list":
+        response = head_list(api_client)
+    else:
+        signup_group = SignUpGroupFactory(registration=registration)
+        response = head_detail(api_client, pk=signup_group.pk)
+
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
