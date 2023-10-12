ef163e52926f68e5f7a9815120befc54597f8e26
Harri Rissanen <harri.rissanen@vincit.fi>
2023-10-12 16:32:53 +0300

    Add audit log app, model and main settings
    LINK-1640

---
+++ audit_log/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class AuditLogEntry(models.Model):
+    is_sent = models.BooleanField(default=False, verbose_name=_("is sent"))
+    message = models.JSONField(verbose_name=_("message"))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("created at"))
