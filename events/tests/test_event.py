56021928953fac067bce74c61d76119f4fb1e7a4
Matti Eiden <matti.eiden@anders.com>
2023-11-06 14:17:29 +0200

    fix: ensure last_modified_time is updated when an event is saved
    Especially in case of soft deletion by importers
    save(update_fields=[...]) would be called, which leaves
    last_modified_time untouched. This commit changes the updating of
    last_modified_time as opt-out. The last_modified_time is not updated
    when n_events is updated.

--- events/tests/test_event.py
+++ events/tests/test_event.py
@@ -1,3 +1,6 @@
+import operator
+from typing import Callable
+
 import pytest
 
 
@@ -34,3 +37,21 @@ def test_prevent_circular_event_replacement(event, event2, event3):
     event3.replaced_by = event
     with pytest.raises(Exception):
         event.save()
+
+
+@pytest.mark.no_test_audit_log
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "skip_last_modified_time,compare_time,compare_user",
+    [[False, operator.gt, operator.is_], [True, operator.eq, operator.is_not]],
+)
+def test_event_update_fields_updates_last_modified_time(
+    event, skip_last_modified_time, compare_time: Callable, compare_user: Callable
+):
+    old_last_modified_time = event.last_modified_time
+    event.name = "New name"
+    event.save(update_fields=["name"], skip_last_modified_time=skip_last_modified_time)
+    event.refresh_from_db()
+
+    assert compare_time(event.last_modified_time, old_last_modified_time)
+    assert compare_user(event.last_modified_by_id, None)
