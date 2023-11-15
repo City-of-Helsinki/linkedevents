40d68f814df2a843e0ae123bbff8846aebd613d2
Tuomas Haapala <tuomas.haapala@anders.fi>
2023-11-15 21:23:07 +0200

    build: add commit message format requirements
    From now on, all new commit messages must adhere to the conventional
    commits v1.0.0 spec and their max line length is limited to 72
    characters. The rules are enforced with commitlint, which is run as a
    pre-commit hook and also as a stage in the GitHub CI pipeline.

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+const Configuration = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 72],
+    'body-leading-blank': [2, 'always'],
+  },
+};
+
+module.exports = Configuration;
