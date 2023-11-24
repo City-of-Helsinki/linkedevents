d5aaf30b5b37fd3eb60c733d30a29b000040a5e6
Juha Louhiranta <juha.louhiranta@anders.fi>
2023-11-24 12:13:36 +0200

    feat: lower LargeResultsSetPagination.max_page_size
    Refs: LINK-1728

--- events/api_pagination.py
+++ events/api_pagination.py
@@ -24,5 +24,4 @@ class CustomPagination(pagination.PageNumberPagination):
 
 class LargeResultsSetPagination(CustomPagination):
     page_size = 1000
-    page_size_query_param = "page_size"
-    max_page_size = 10000
+    max_page_size = 1000
