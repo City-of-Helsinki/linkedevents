54eed975bc9aa00111d6022204062a64ffed30f9
Harri Rissanen <harri.rissanen@vincit.fi>
2024-02-27 14:47:04 +0200

    fix: use fallback languages in ICS file generation
    Refs: LINK-1910

--- registrations/tests/test_utils.py
+++ registrations/tests/test_utils.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
+from unittest.mock import patch
 
 import pytest
+from django.utils import translation
 from django.utils.timezone import localtime
 
 from events.tests.factories import EventFactory, PlaceFactory
@@ -43,12 +45,16 @@ def test_create_ics_file_content():
         start_time=localtime(),
         end_time=localtime() + timedelta(days=10),
     )
-    (filename, ics) = create_event_ics_file_content(event)
+    filename, ics = create_event_ics_file_content(event)
 
     assert filename == "event_helsinki:123.ics"
     assert (
         ics
-        == b"BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//linkedevents.hel.fi//NONSGML API//EN\r\nBEGIN:VEVENT\r\nSUMMARY:Event name\r\nDTSTART;TZID=Europe/Helsinki:20240101T020000\r\nDTEND;TZID=Europe/Helsinki:20240111T020000\r\nDESCRIPTION:Event description\r\nLOCATION:Place name\\, Streen address\\, Helsinki\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n"
+        == b"BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//linkedevents.hel.fi//NONSGML "
+        b"API//EN\r\nBEGIN:VEVENT\r\nSUMMARY:Event "
+        b"name\r\nDTSTART;TZID=Europe/Helsinki:20240101T020000\r\nDTEND;TZID=Europe/Helsinki"
+        b":20240111T020000\r\nDESCRIPTION:Event description\r\nLOCATION:Place name\\, "
+        b"Streen address\\, Helsinki\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n"
     )
 
 
@@ -67,10 +73,89 @@ def test_create_ics_file_content_with_start_time_as_end_time():
         start_time=localtime(),
         end_time=None,
     )
-    (filename, ics) = create_event_ics_file_content(event)
+    filename, ics = create_event_ics_file_content(event)
 
     assert filename == "event_helsinki:123.ics"
     assert (
         ics
-        == b"BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//linkedevents.hel.fi//NONSGML API//EN\r\nBEGIN:VEVENT\r\nSUMMARY:Event name\r\nDTSTART;TZID=Europe/Helsinki:20240101T020000\r\nDTEND;TZID=Europe/Helsinki:20240101T020000\r\nDESCRIPTION:Event description\r\nLOCATION:Place name\\, Streen address\\, Helsinki\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n"
+        == b"BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//linkedevents.hel.fi//NONSGML "
+        b"API//EN\r\nBEGIN:VEVENT\r\nSUMMARY:Event name\r\nDTSTART;TZID=Europe/"
+        b"Helsinki:20240101T020000\r\nDTEND;TZID=Europe/Helsinki"
+        b":20240101T020000\r\nDESCRIPTION:Event description\r\nLOCATION:Place name\\, "
+        b"Streen address\\, Helsinki\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n"
     )
+
+
+@pytest.mark.parametrize(
+    "language,name_value,expected_fallback_language",
+    [
+        ("fi", "Event name", "en"),
+        ("fi", "Evenemangets namn", "sv"),
+        ("sv", "Event name", "en"),
+        ("sv", "Tapahtuman nimi", "fi"),
+        ("en", "Tapahtuman nimi", "fi"),
+        ("en", "Evenemangets namn", "sv"),
+    ],
+)
+@pytest.mark.django_db
+def test_create_ics_file_using_fallback_languages(
+    language, name_value, expected_fallback_language
+):
+    with translation.override(expected_fallback_language):
+        event = EventFactory(
+            id="helsinki:123",
+            name=name_value,
+            short_description_fi="Event description",
+            location=PlaceFactory(
+                name_fi="Place name",
+                street_address_fi="Streen address",
+                address_locality_fi="Helsinki",
+            ),
+            start_time=localtime(),
+            end_time=localtime() + timedelta(days=10),
+        )
+
+    assert getattr(event, f"name_{language}") is None
+    assert getattr(event, f"name_{expected_fallback_language}") == name_value
+
+    filename, ics = create_event_ics_file_content(event, language=language)
+
+    assert filename == "event_helsinki:123.ics"
+    assert f"SUMMARY:{name_value}" in str(ics)
+
+
+@pytest.mark.parametrize(
+    "language,expected_fallback_language",
+    [
+        ("fi", "en"),
+        ("sv", "en"),
+        ("en", "fi"),
+    ],
+)
+@pytest.mark.django_db
+def test_create_ics_file_correct_fallback_language_used_if_event_name_is_none(
+    language, expected_fallback_language
+):
+    event = EventFactory(
+        id="helsinki:123",
+        name=None,
+        short_description_fi="Event description",
+        location=PlaceFactory(
+            name_fi="Place name",
+            street_address_fi="Streen address",
+            address_locality_fi="Helsinki",
+        ),
+        start_time=localtime(),
+        end_time=localtime() + timedelta(days=10),
+    )
+
+    assert getattr(event, f"name_{language}") is None
+    assert getattr(event, f"name_{expected_fallback_language}") is None
+
+    with pytest.raises(
+        ValueError,
+        match="Event doesn't have start_time or name. Ics file cannot be created.",
+    ), patch("django.utils.translation.override") as mocked_translation_override:
+        create_event_ics_file_content(event, language=language)
+
+        mocked_translation_override.assert_called_with(expected_fallback_language)
