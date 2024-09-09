b503a7e8bc8dd99c02345e70cb799be2abb2bd6e
Harri Rissanen <harri.rissanen@vincit.fi>
2024-09-09 16:12:42 +0300

    feat(events): use noreply address for feedback
    Refs: LINK-1729

--- events/tests/test_feedback_form.py
+++ events/tests/test_feedback_form.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from audit_log.models import AuditLogEntry
 from events.models import Feedback
 from events.tests.utils import versioned_reverse as reverse
+from registrations.utils import get_email_noreply_address
 
 load = {
     "name": "Launchpad McQuack",
@@ -16,37 +17,41 @@ load = {
 
 
 @pytest.mark.django_db
-def test__signed_in_user_submits(api_client, user):
+def test_signed_in_user_submits(api_client, user):
     signed_url = reverse("feedback-list")
     response = api_client.post(signed_url, load, format="json")
     assert response.status_code == 401
 
     api_client.force_authenticate(user=user)
     response = api_client.post(signed_url, load, format="json")
+    assert response.status_code == status.HTTP_201_CREATED
+
     assert Feedback.objects.all().count() == 1
     assert len(mail.outbox) == 1
     assert (
         mail.outbox[0].subject
         == f"[LinkedEvents] {load['subject']} reported by {load['name']}"
     )
-    assert mail.outbox[0].body == load["body"]
+    assert mail.outbox[0].body == f"Email: {load['email']}, message: {load['body']}"
     assert mail.outbox[0].to == [settings.SUPPORT_EMAIL]
-    assert mail.outbox[0].from_email == load["email"]
+    assert mail.outbox[0].from_email == get_email_noreply_address()
 
 
 @pytest.mark.django_db
-def test__guest_user_submits(api_client):
+def test_guest_user_submits(api_client):
     guest_url = reverse("guest-feedback-list")
-    api_client.post(guest_url, load, format="json")
+    response = api_client.post(guest_url, load, format="json")
+    assert response.status_code == status.HTTP_201_CREATED
+
     assert Feedback.objects.all().count() == 1
     assert len(mail.outbox) == 1
     assert (
         mail.outbox[0].subject
         == f"[LinkedEvents] {load['subject']} reported by {load['name']}"
     )
-    assert mail.outbox[0].body == load["body"]
+    assert mail.outbox[0].body == f"Email: {load['email']}, message: {load['body']}"
     assert mail.outbox[0].to == [settings.SUPPORT_EMAIL]
-    assert mail.outbox[0].from_email == load["email"]
+    assert mail.outbox[0].from_email == get_email_noreply_address()
 
 
 @pytest.mark.django_db
