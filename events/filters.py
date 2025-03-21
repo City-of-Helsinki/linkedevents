5ff57221875c102d15b6d5ef9513623ba56e56d3
Matti Eiden <matti.eiden@anders.com>
2025-03-21 08:04:31 +0200

    refactor: move days, start and end from api to filters

--- events/filters.py
+++ events/filters.py
@@ -6,6 +6,7 @@ from typing import Iterable, Optional
 from zoneinfo import ZoneInfo
 
 import django_filters
+import pytz
 from django.conf import settings
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
@@ -29,6 +30,7 @@ from munigeo.api import srid_to_srs
 from rest_framework import serializers
 from rest_framework.exceptions import ParseError
 
+from events import utils
 from events.models import Event, EventAggregate, Place
 from events.search_index.utils import extract_word_bases
 from events.widgets import DistanceWithinWidget
@@ -327,6 +329,32 @@ class EventFilter(django_filters.rest_framework.FilterSet):
         help_text=_("Filter events by their occurring weekday using iso format."),
     )
 
+    days = django_filters.CharFilter(
+        method="filter_days",
+        help_text=_(
+            "Search for events that intersect with the current time and specified "
+            "amount of days from current time."
+        ),
+    )
+
+    start = django_filters.CharFilter(
+        method="filter_start",
+        help_text=_(
+            "Search for events beginning or ending after this time. Dates can be "
+            "specified using ISO 8601 (for example, '2024-01-12') and additionally "
+            "`today` and `now`."
+        ),
+    )
+
+    end = django_filters.CharFilter(
+        method="filter_end",
+        help_text=_(
+            "Search for events beginning or ending before this time. Dates can be "
+            "specified using ISO 8601 (for example, '2024-01-12') and additionally "
+            "`today` and `now`."
+        ),
+    )
+
     class Meta:
         model = Event
         fields = {
@@ -511,6 +539,54 @@ class EventFilter(django_filters.rest_framework.FilterSet):
             return self.request.user.get_editable_events_for_registration(queryset)
         else:
             return queryset.none()
+
+    def filter_days(self, queryset, name, value):
+        try:
+            days = int(value)
+        except ValueError:
+            raise ParseError(_("Error while parsing days."))
+        if days < 1:
+            raise serializers.ValidationError(_("Days must be 1 or more."))
+        if "start" in self.data or "end" in self.data:
+            raise serializers.ValidationError(
+                _("Start or end cannot be used with days.")
+            )
+
+        today = datetime.now(datetime_timezone.utc).date()
+
+        start = today.isoformat()
+        end = (today + timedelta(days=days)).isoformat()
+
+        queryset = self.filter_start(queryset, "start", start)
+        queryset = self.filter_end(queryset, "end", end)
+        return queryset
+
+    def filter_start(self, queryset, name, value):
+        if "end" not in self.data:
+            # postponed events are considered to be "far" in the future and should be
+            # included if end is *not* given
+            postponed_q = Q(event_status=Event.Status.POSTPONED)
+        else:
+            postponed_q = Q()
+
+        # Inconsistent behaviour, see test_inconsistent_tz_default
+        dt = utils.parse_time(value, default_tz=pytz.timezone(settings.TIME_ZONE))[0]
+
+        # only return events with specified end times, or unspecified start times, during the whole of the event  # noqa: E501
+        # this gets of rid pesky one-day events with no known end time (but known
+        # start) after they started
+        return queryset.filter(
+            Q(end_time__gt=dt, has_end_time=True)
+            | Q(end_time__gt=dt, has_start_time=False)
+            | Q(start_time__gte=dt)
+            | postponed_q
+        )
+
+    def filter_end(self, queryset, name, value):
+        dt = utils.parse_end_time(value, default_tz=pytz.timezone(settings.TIME_ZONE))[
+            0
+        ]
+        return queryset.filter(Q(end_time__lt=dt) | Q(start_time__lte=dt))
 
 
 class OrganizationFilter(django_filters.rest_framework.FilterSet):
