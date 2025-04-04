15e2d85c1be3982192d5e756009f6e36e6edd94f
Mika Hietanen <m.hietanen84@gmail.com>
2025-04-04 15:06:35 +0300

    test: add tests for event index auto-sync
    Refs: LINK-2269

--- events/tests/management/test_rebuild_event_search_index.py
+++ events/tests/management/test_rebuild_event_search_index.py
@@ -41,3 +41,71 @@ def test_rebuild_search_vectors_with_clean(event, place, data_source):
     second_event.delete()
     call_command("rebuild_event_search_index", clean=True)
     assert EventSearchIndex.objects.all().count() == 1
+
+
+@pytest.mark.django_db(transaction=True)
+def test_auto_update_event_search_index_on_event_create(
+    event, place, keyword, data_source
+):
+    event.keywords.add(keyword)
+    event.save()
+    assert EventSearchIndex.objects.all().count() == 1
+    event_full_text = EventSearchIndex.objects.first()
+    assert event_full_text.event == event
+    assert event_full_text.place == place
+    assert event_full_text.event_last_modified_time == event.last_modified_time
+    assert event_full_text.place_last_modified_time == place.last_modified_time
+    assert event_full_text.search_vector_fi is not None
+    assert event_full_text.search_vector_sv is not None
+    assert event_full_text.search_vector_en is not None
+    event_2 = EventFactory(id="test:2", data_source=data_source, origin_id=2)
+    event_2.keywords.add(keyword)
+    event_2.save()
+    assert EventSearchIndex.objects.all().count() == 2
+    event_2_full_text = EventSearchIndex.objects.get(event=event_2)
+    assert event_2_full_text.event == event_2
+    assert event_2_full_text.search_vector_fi is not None
+    assert event_2_full_text.search_vector_sv is not None
+    assert event_2_full_text.search_vector_en is not None
+
+
+@pytest.mark.django_db(transaction=True)
+def test_auto_update_event_search_index_on_event_update(
+    event, place, keyword, data_source
+):
+    event.keywords.add(keyword)
+    event.save()
+    assert EventSearchIndex.objects.all().count() == 1
+    event_full_text = EventSearchIndex.objects.first()
+    assert event_full_text.event == event
+    assert event_full_text.place == place
+    assert event_full_text.event_last_modified_time == event.last_modified_time
+    assert event_full_text.place_last_modified_time == place.last_modified_time
+    assert event_full_text.search_vector_fi is not None
+    assert event_full_text.search_vector_sv is not None
+    assert event_full_text.search_vector_en is not None
+    event.name_fi = "Päivitetty nimi"
+    event.save()
+    assert EventSearchIndex.objects.all().count() == 1
+    updated_event_full_text = EventSearchIndex.objects.first()
+    assert updated_event_full_text.event == event
+    assert updated_event_full_text.place == place
+    assert updated_event_full_text.search_vector_fi is not None
+    assert updated_event_full_text.search_vector_sv is not None
+    assert updated_event_full_text.search_vector_en is not None
+    assert event.name_fi[:4].lower() in str(updated_event_full_text.search_vector_fi)
+
+
+@pytest.mark.django_db(transaction=True)
+def test_auto_update_event_search_index_on_event_delete(
+    event, place, keyword, data_source
+):
+    event.keywords.add(keyword)
+    event.save()
+    assert EventSearchIndex.objects.all().count() == 1
+    event_2 = EventFactory(id="test:2", data_source=data_source, origin_id=2)
+    event_2.keywords.add(keyword)
+    event_2.save()
+    assert EventSearchIndex.objects.all().count() == 2
+    event.delete()
+    assert EventSearchIndex.objects.all().count() == 1
