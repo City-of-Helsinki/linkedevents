c42ac121e96b1a47c5cc0abf8074679dce57a401
Mika Hietanen <m.hietanen84@gmail.com>
2025-05-07 08:48:52 +0300

    refactor: simplify language code extraction
    Refs: LINK-2302

--- linkedevents/utils.py
+++ linkedevents/utils.py
@@ -1,26 +1,23 @@
 from django.conf import settings
 from rest_framework import serializers
-from rest_framework.routers import APIRootView
-from rest_framework.views import get_view_name as original_get_view_name
 
 
 def get_view_name(view):
+    from rest_framework.routers import APIRootView
+    from rest_framework.views import get_view_name as original_get_view_name
+
     if type(view) is APIRootView:
         return getattr(settings, "INSTANCE_NAME", "Linked Events")
     return original_get_view_name(view)
 
 
-def get_fixed_lang_codes():
-    lang_codes = []
-
-    for language in settings.LANGUAGES:
-        lang_code = language[0]
-        lang_code = lang_code.replace(
-            "-", "_"
-        )  # to handle complex codes like e.g. zh-hans
-        lang_codes.append(lang_code)
-
-    return lang_codes
+def get_fixed_lang_codes() -> list[str]:
+    """
+    Returns a list of language codes.
+    Replaces "-" with "_" in the language codes.
+    :return: a list of language codes
+    """
+    return [lang_code[0].replace("-", "_") for lang_code in settings.LANGUAGES]
 
 
 def validate_serializer_field_for_duplicates(values, field, error_detail_callback):
