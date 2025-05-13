ff5389fb1d85fe6e2d5046c70cbb2333794459b5
Nico Virkki <nico.virkki@anders.com>
2025-05-13 15:21:00 +0300

    feat: delete EventSearchIndex in batches
    Delete the EventSearchIndex in batches to make it a bit faster.
    
    Refs: LINK-2305

--- events/management/commands/rebuild_event_search_index.py
+++ events/management/commands/rebuild_event_search_index.py
@@ -1,9 +1,12 @@
 import logging
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 from events.models import EventSearchIndex
 from events.search_index.postgres import EventSearchIndexService
+from events.search_index.utils import batch_qs
 
 logger = logging.getLogger(__name__)
 
@@ -13,7 +16,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         logger.info("Cleaning the search index...")
-        EventSearchIndex.objects.all().delete()
-        logger.info("Cleaned the search index.")
+        self.delete_index_in_batches()
         EventSearchIndexService.bulk_update_search_indexes()
         EventSearchIndexService.update_index_search_vectors()
+
+    @transaction.atomic
+    def delete_index_in_batches(self):
+        for _, _, _, qs in batch_qs(
+            EventSearchIndex.objects.all(),
+            batch_size=settings.EVENT_SEARCH_INDEX_REBUILD_BATCH_SIZE,
+        ):
+            ids = qs.values_list("event_id", flat=True)
+            EventSearchIndex.objects.filter(event_id__in=ids).delete()
