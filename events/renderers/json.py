cc3c888e078a931aab3b4a8635b6c0e5e24f8e3c
Juha Louhiranta <juha.louhiranta@anders.com>
2025-11-27 16:36:53 +0200

    feat: remove unused UJSONRenderer
    Refs: LINK-2339

--- events/renderers/json.py
+++ events/renderers/json.py
@@ -1,4 +1,3 @@
-import ujson
 from rest_framework import renderers
 
 
@@ -13,11 +12,3 @@ class JSONLDRenderer(JSONRenderer):
     media_type = "application/ld+json"
     format = "json-ld"
     charset = "utf-8"
-
-
-class UJSONRenderer(renderers.BaseRenderer):
-    media_type = "application/json"
-    format = "json"
-
-    def render(self, data, accepted_media_type=None, renderer_context=None):
-        return ujson.dumps(data, escape_forward_slashes=False)
