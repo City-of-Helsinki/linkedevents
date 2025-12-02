a63f626948ebea60028efa8eb223346434544b63
Daniel Prange <daniel.prange@anders.com>
2025-12-02 07:01:23 +0200

    feat: add 13,5% vat percentage
    Refs: LINK-2438

--- registrations/models.py
+++ registrations/models.py
@@ -78,12 +78,14 @@ web_store_price_group_meta_key = "signupPriceGroupId"
 VAT_PERCENTAGES = (
     (VatPercentage.VAT_25_5.value, "25.5 %"),
     (VatPercentage.VAT_14.value, "14 %"),
+    (VatPercentage.VAT_13_5.value, "13.5 %"),
     (VatPercentage.VAT_10.value, "10 %"),
     (VatPercentage.VAT_0.value, "0 %"),
 )
 VAT_CODE_MAPPING = {
     VatPercentage.VAT_25_5.value: "47",
     VatPercentage.VAT_14.value: "45",
+    VatPercentage.VAT_13_5.value: "48",
     VatPercentage.VAT_10.value: "46",
     VatPercentage.VAT_0.value: "4U",
 }
