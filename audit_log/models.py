eca95ea252f331d7240ad824b09bd4a80d9e1676
Juha Louhiranta <juha.louhiranta@anders.com>
2026-01-27 16:21:01 +0200

    feat: remove old AuditLogEntry model
    Backend now uses django-resilient-logger
    for audit logging.
    
    Refs: LINK-2339

--- audit_log/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-
-class AuditLogEntry(models.Model):
-    is_sent = models.BooleanField(default=False, verbose_name=_("is sent"))
-    message = models.JSONField(verbose_name=_("message"))
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("created at"))
